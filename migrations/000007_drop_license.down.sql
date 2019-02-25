10286b9fa61aa6ff6a00e88ed455d25a9473622d
Channing Kimble-Brown <channing@golang.org>
2019-02-25 14:23:22 -0500

    migrations: drop license column and add not null constraint to license_name
    Removing the license column in the database schema and adding the NOT NULL
    constraint to the license_name column. This is all part of an effort to
    merge the ReadMe and License structs into the Version struct so that
    Version will reflect the new flattened Versions table in the database.
    
    Change-Id: If24943492d6700719622c0537504137d12fa322f
    Reviewed-on: https://team-review.git.corp.google.com/c/422453
    Reviewed-by: Julie Qiu <julieqiu@google.com>

---
+++ migrations/000007_drop_license.down.sql
@@ -0,0 +1,6 @@
+-- Copyright 2009 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+ALTER TABLE versions ALTER COLUMN license_name DROP NOT NULL;
+ALTER TABLE versions ADD COLUMN license TEXT NOT NULL;
---
+++ migrations/000007_drop_license.up.sql
@@ -0,0 +1,6 @@
+-- Copyright 2009 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+ALTER TABLE versions ALTER COLUMN license_name SET NOT NULL;
+ALTER TABLE versions DROP COLUMN license;
