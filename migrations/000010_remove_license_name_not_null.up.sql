f75d56171195db06cd8ac40862f969e8afef090f
Channing Kimble-Brown <channing@golang.org>
2019-03-15 13:39:04 -0400

    migrations: adding and fixing licenses
    Some of the migrations files were missing licenses or said
    'Copyright 2009' instead of 'Copyright 2019' and needed to be
    fixed.
    
    Change-Id: I36a6c627b8893ad227b608ea96d4f257e9ab0562
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/435003
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- migrations/000010_remove_license_name_not_null.up.sql
+++ migrations/000010_remove_license_name_not_null.up.sql
@@ -1,1 +1,5 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
 ALTER TABLE versions ALTER COLUMN license DROP NOT NULL;
