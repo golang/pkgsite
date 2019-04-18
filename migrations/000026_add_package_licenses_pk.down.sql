55dc997d92c67a36a5bbdba536bc95199cfaccae
Rob Findley <rfindley@google.com>
2019-04-18 15:37:42 -0400

    migrations: add a primary key to package_licenses
    This was an embarassing omission on my part, that I did not catch
    because I was continually fetching packages for the first time. This
    fixes the bug of duplicate licenses being associated with a package
    after multiple fetches of the same version.
    
    Updates b/129000846
    
    Change-Id: I1e3eb594cbf73010dacf9d5d2f4237ce7463ff60
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/451313
    Reviewed-by: Julie Qiu <julieqiu@google.com>

---
+++ migrations/000026_add_package_licenses_pk.down.sql
@@ -0,0 +1,9 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE package_licenses DROP CONSTRAINT package_licenses_pkey;
+
+END;
