d2093c3d8112715db123a6c422f39f26649e1efa
Rob Findley <rfindley@google.com>
2019-06-18 15:30:10 -0400

    migrations: add new columns for our schema migration
    Add license columns directly to the packages table, so that we can
    eventually deprecate the package_licenses table. Add v1_path so that we
    can remove the need for the module and series tables.
    
    Updates b/135546160
    
    Change-Id: I47ae1b6b31817508a82d0515d41e5c466e9391f5
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/490515
    Reviewed-by: Julie Qiu <julieqiu@google.com>

---
+++ migrations/000048_add_columns_to_packages.down.sql
@@ -0,0 +1,12 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE packages
+  DROP COLUMN license_types,
+  DROP COLUMN license_paths,
+  DROP COLUMN v1_path;
+
+END;
