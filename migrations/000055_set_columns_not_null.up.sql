868b8e0f56018d3f5625a083a4781b226f77e549
Julie Qiu <julie@golang.org>
2019-06-19 21:26:01 -0400

    migrations: set versions not null columns
    Change-Id: Ib8df8fe12e39e64ff32a73aa60dcd53fa1e2fcae
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/492160
    Reviewed-by: Robert Findley <rfindley@google.com>

---
+++ migrations/000055_set_columns_not_null.up.sql
@@ -0,0 +1,11 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE versions ALTER COLUMN major SET NOT NULL;
+ALTER TABLE versions ALTER COLUMN minor SET NOT NULL;
+ALTER TABLE versions ALTER COLUMN patch SET NOT NULL;
+
+END;
