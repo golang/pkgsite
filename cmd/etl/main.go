95faf7c1f72f626c41e8965a6fa472b2fac4d8da
Rob Findley <rfindley@google.com>
2019-07-02 16:09:04 -0400

    internal/config: add more shared configuration
    Common patterns for resolving URLs, Addrs, and DB connection strings
    are extracted to the config package.
    
    Change-Id: Id0538f4cd557fa390693bff64618c743e43261af
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/499880
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- cmd/etl/main.go
+++ cmd/etl/main.go
@@ -9,7 +9,6 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -25,7 +24,6 @@ import (
 	"golang.org/x/discovery/internal/middleware"
 	"golang.org/x/discovery/internal/postgres"
 	"golang.org/x/discovery/internal/proxy"
-	"golang.org/x/discovery/internal/secrets"
 	"google.golang.org/appengine"
 
 	"contrib.go.opencensus.io/integrations/ocsql"
@@ -33,10 +31,8 @@ import (
 )
 
 var (
-	indexURL   = getEnv("GO_MODULE_INDEX_URL", "https://index.golang.org/index")
-	proxyURL   = getEnv("GO_MODULE_PROXY_URL", "https://proxy.golang.org")
-	timeout    = getEnv("GO_DISCOVERY_CRON_TIMEOUT_MINUTES", "10")
-	queueName  = getEnv("GO_DISCOVERY_CRON_TASK_QUEUE", "dev-fetch-tasks")
+	timeout    = config.GetEnv("GO_DISCOVERY_CRON_TIMEOUT_MINUTES", "10")
+	queueName  = config.GetEnv("GO_DISCOVERY_CRON_TASK_QUEUE", "dev-fetch-tasks")
 	workers    = flag.Int("workers", 10, "number of concurrent requests to the fetch service, when running locally")
 	staticPath = flag.String("static", "content/static", "path to folder containing static files served")
 )
@@ -45,7 +41,7 @@ func main() {
 	flag.Parse()
 
 	ctx := context.Background()
-	dbinfo, err := dbConnInfo(ctx)
+	dbinfo, err := config.DBConnInfo(ctx)
 	if err != nil {
 		log.Fatalf("Unable to construct database connection info string: %v", err)
 	}
@@ -55,34 +51,23 @@ func main() {
 		log.Fatalf("unable to register our ocsql driver: %v\n", err)
 	}
 	db, err := postgres.Open(driverName, dbinfo)
-	db.Driver()
 	if err != nil {
 		log.Fatalf("postgres.Open(%q): %v", dbinfo, err)
 	}
 	defer db.Close()
 
-	indexClient, err := index.New(indexURL)
+	indexClient, err := index.New(config.IndexURL())
 	if err != nil {
-		log.Fatalf("index.New(%q): %v", indexURL, err)
+		log.Fatalf("index.New: %v", err)
 	}
-
-	proxyClient, err := proxy.New(proxyURL)
+	proxyClient, err := proxy.New(config.ProxyURL())
 	if err != nil {
-		log.Fatalf("proxy.New(%q): %v", proxyURL, err)
+		log.Fatalf("proxy.New: %v", err)
 	}
 
 	templatePath := filepath.Join(*staticPath, "html/cron/index.tmpl")
 	indexTemplate, err := template.New("index.tmpl").Funcs(template.FuncMap{
-		"truncate": func(length int, text *string) *string {
-			if text == nil {
-				return nil
-			}
-			if len(*text) <= length {
-				return text
-			}
-			s := (*text)[:length] + "..."
-			return &s
-		},
+		"truncate": truncate,
 	}).ParseFiles(templatePath)
 	if err != nil {
 		log.Fatalf("template.ParseFiles(%q): %v", templatePath, err)
@@ -105,17 +90,6 @@ func main() {
 	}
 	go http.ListenAndServe(config.DebugAddr("localhost:8001"), dcensusServer)
 
-	// Default to addr on localhost to mute security popup about incoming
-	// network connections when running locally. When running in prod, App
-	// Engine requires that the app listens on the port specified by the
-	// environment variable PORT.
-	var addr string
-	if port := os.Getenv("PORT"); port != "" {
-		addr = fmt.Sprintf(":%s", port)
-	} else {
-		addr = "localhost:8000"
-	}
-
 	handlerTimeout, err := strconv.Atoi(timeout)
 	if err != nil {
 		log.Fatalf("strconv.Atoi(%q): %v", timeout, err)
@@ -125,34 +99,19 @@ func main() {
 	if os.Getenv("GAE_ENV") == "standard" {
 		appengine.Main()
 	} else {
+		addr := config.HostAddr("localhost:8000")
 		log.Printf("Listening on addr %s", addr)
 		log.Fatal(http.ListenAndServe(addr, nil))
 	}
 }
 
-func dbConnInfo(ctx context.Context) (string, error) {
-	var (
-		user     = getEnv("GO_DISCOVERY_DATABASE_USER", "postgres")
-		password = getEnv("GO_DISCOVERY_DATABASE_PASSWORD", "")
-		host     = getEnv("GO_DISCOVERY_DATABASE_HOST", "localhost")
-		dbname   = getEnv("GO_DISCOVERY_DATABASE_NAME", "discovery-database")
-	)
-
-	// When running on App Engine, the runtime sets GAE_ENV to 'standard' per
-	// https://cloud.google.com/appengine/docs/standard/go111/runtime
-	if os.Getenv("GAE_ENV") == "standard" {
-		var err error
-		password, err = secrets.Get(ctx, "go_discovery_database_password_etl")
-		if err != nil {
-			return "", fmt.Errorf("could not get database password secret: %v", err)
-		}
+func truncate(length int, text *string) *string {
+	if text == nil {
+		return nil
 	}
-	return fmt.Sprintf("user='%s' password='%s' host='%s' dbname='%s' sslmode=disable", user, password, host, dbname), nil
-}
-
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
+	if len(*text) <= length {
+		return text
 	}
-	return fallback
+	s := (*text)[:length] + "..."
+	return &s
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -4,13 +4,23 @@
 
 // Package config provides facilities for resolving discovery configuration
 // parameters from the hosting environment.
-// TODO(rfindley): factor out more functionality here.
 package config
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"golang.org/x/discovery/internal/secrets"
 )
+
+// HostAddr returns the network on which to serve the primary HTTP service.
+func HostAddr(dflt string) string {
+	if port := os.Getenv("PORT"); port != "" {
+		return fmt.Sprintf(":%s", port)
+	}
+	return dflt
+}
 
 // DebugAddr returns the network address on which to serve debugging
 // information.
@@ -19,4 +29,45 @@ func DebugAddr(dflt string) string {
 		return fmt.Sprintf(":%s", port)
 	}
 	return dflt
+}
+
+// GetEnv looks up the given key from the environment, returning its value if
+// it exists, and otherwise returning the given fallback value.
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+// IndexURL returns the URL of the Go module index.
+func IndexURL() string {
+	return GetEnv("GO_MODULE_INDEX_URL", "https://index.golang.org/index")
+}
+
+// ProxyURL returns the URL of the Go module proxy.
+func ProxyURL() string {
+	return GetEnv("GO_MODULE_PROXY_URL", "https://proxy.golang.org")
+}
+
+// DBConnInfo returns a PostgreSQL connection string constructed from
+// environment variables.
+func DBConnInfo(ctx context.Context) (string, error) {
+	var (
+		user     = GetEnv("GO_DISCOVERY_DATABASE_USER", "postgres")
+		password = GetEnv("GO_DISCOVERY_DATABASE_PASSWORD", "")
+		host     = GetEnv("GO_DISCOVERY_DATABASE_HOST", "localhost")
+		dbname   = GetEnv("GO_DISCOVERY_DATABASE_NAME", "discovery-database")
+	)
+
+	// When running on App Engine, the runtime sets GAE_ENV to 'standard' per
+	// https://cloud.google.com/appengine/docs/standard/go111/runtime
+	if os.Getenv("GAE_ENV") == "standard" {
+		var err error
+		password, err = secrets.Get(ctx, "go_discovery_database_password_etl")
+		if err != nil {
+			return "", fmt.Errorf("could not get database password secret: %v", err)
+		}
+	}
+	return fmt.Sprintf("user='%s' password='%s' host='%s' dbname='%s' sslmode=disable", user, password, host, dbname), nil
 }
