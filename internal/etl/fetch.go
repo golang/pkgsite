fca7674c63220d44ea2c63b0633c39e75d5e5b61
Jonathan Amsterdam <jba@google.com>
2019-08-09 08:41:04 -0400

    internal/etl: add bug for UpsertVersionState split TODO
    Change-Id: I403d3dd0d7580d42653319c514f69c96314c7bf7
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/524052
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- internal/etl/fetch.go
+++ internal/etl/fetch.go
@@ -102,8 +102,8 @@ func fetchAndUpdateState(ctx context.Context, modulePath, version string, client
 	// module@version. This must happen last, because if it succeeds with a
 	// code < 500 but a later action fails, we will never retry the later action.
 
-	// TODO(jba): Split UpsertVersionState into InsertVersionState and UpdateVersionState.
-	// See 	if err := db.UpsertVersionState(ctx, modulePath, version, appVersionLabel, time.Time{}, code, fetchErr); err != nil {
+	// TODO(b/139178863): Split UpsertVersionState into InsertVersionState and UpdateVersionState.
+	if err := db.UpsertVersionState(ctx, modulePath, version, appVersionLabel, time.Time{}, code, fetchErr); err != nil {
 		log.Printf("db.UpsertVersionState(ctx, %q, %q, %q, %q, %v): %q", modulePath, version, config.AppVersionLabel(), code, fetchErr, err)
 		if fetchErr != nil {
 			err = fmt.Errorf("error updating version state: %v, original error: %v", err, fetchErr)
