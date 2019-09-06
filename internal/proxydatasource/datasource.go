df207158825a5948d25646395c4d11ca11c672b3
Rob Findley <rfindley@google.com>
2019-09-06 12:55:16 -0400

    internal/proxydatasource: continue searching for module on HTTP 410
    The proxy returns 410, rather than 404, when a module path is not found.
    
    Change-Id: I2fed459cb18373ed16c2fc7869886bcf62f4543c
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/544157
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- internal/proxydatasource/datasource.go
+++ internal/proxydatasource/datasource.go
@@ -272,7 +272,7 @@ func (ds *DataSource) findModule(ctx context.Context, pkgPath string, version st
 	pkgPath = strings.TrimLeft(pkgPath, "/")
 	for modulePath := pkgPath; modulePath != "" && modulePath != "."; modulePath = path.Dir(modulePath) {
 		info, err := ds.proxyClient.GetInfo(ctx, modulePath, version)
-		if xerrors.Is(err, derrors.NotFound) {
+		if xerrors.Is(err, derrors.NotFound) || xerrors.Is(err, derrors.Gone) {
 			continue
 		}
 		if err != nil {
