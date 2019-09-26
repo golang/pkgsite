f876e495d0c9c45350043cac48b71169c569639e
Rob Findley <rfindley@google.com>
2019-09-26 13:17:24 -0400

    migrations: bucket existing search data
    Now that hll columns are set when inserting search documents, we can
    bulk populate them for existing documents.
    
    Updates b/141182438
    
    Change-Id: I12b8c1449c3c790329e3ddf88e030a9db1346602
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/560266
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000082_apply_hyperloglog_bucketing.up.sql
@@ -0,0 +1,10 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+UPDATE search_documents SET hll_register=hll_hash(package_path) & (128-1);
+UPDATE search_documents SET hll_leading_zeros=hll_zeros(hll_hash(package_path));
+
+END;
