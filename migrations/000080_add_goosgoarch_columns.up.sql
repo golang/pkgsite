065241b9b2af644b025e983a9191122112941726
Jonathan Amsterdam <jba@google.com>
2019-10-02 09:44:30 -0400

    migrations: add GOOS and GOARCH columns for packages
    We want to track the build context we used to parse a package.
    
    Updates b/141852037.
    
    Change-Id: I774bba04e5546bc61aa6fe4d5bf5e9853bddb3dc
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/563755
    Reviewed-by: Julie Qiu <julieqiu@google.com>

---
+++ migrations/000080_add_goosgoarch_columns.up.sql
@@ -0,0 +1,13 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+-- Add columns to record the build context we used to parse the package.
+-- See b/141852037.
+
+BEGIN;
+
+ALTER TABLE packages ADD COLUMN goos text;
+ALTER TABLE packages ADD COLUMN goarch text;
+
+END;
