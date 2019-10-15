b8ec1e61b8305bde6c8f2dee05d0ede5135331a1
Julie Qiu <julie@golang.org>
2019-10-14 22:15:44 -0400

    migrations: update to_tsvector_parent_directories
    The stored procedure to_tsvector_parent_directories is updated to fix a
    bug, where the module path repeats itself in the package path.
    
    Updates b/142392929
    
    Change-Id: Id3b7590345f797be56dcbeff7ec1489f6677f436
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/572986
    CI-Result: Cloud Build <devtools-proctor-result-processor@system.gserviceaccount.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000092_update_to_tsvector_parent_directories.down.sql
@@ -0,0 +1,33 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+-- Compute all directories that exist between modulePath and pkgPath, inclusive
+-- of both modulePath and pkgPath. Return the result as a tsvector.
+CREATE OR REPLACE FUNCTION to_tsvector_parent_directories(package_path TEXT, module_path TEXT)
+RETURNS TSVECTOR AS $$
+  DECLARE
+    sub_path TEXT;
+    parent_directories TEXT := module_path;
+    sub_directories TEXT[][];
+    current_directory TEXT := module_path;
+    tsv_parent_directories TSVECTOR := module_path::tsvector;
+  BEGIN
+    IF package_path = module_path THEN
+      RETURN tsv_parent_directories;
+    END IF;
+
+    sub_path := replace(package_path, module_path || '/', '');
+    sub_directories := regexp_split_to_array(sub_path, '/');
+    FOR i IN 1..cardinality(sub_directories) LOOP
+      current_directory := current_directory || '/' || sub_directories[i];
+      parent_directories = parent_directories || ' ' || current_directory;
+    END LOOP;
+
+    RETURN parent_directories::tsvector;
+END;
+$$ LANGUAGE plpgsql;
+
+END;
