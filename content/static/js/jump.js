b2a44cad06d5bd0bc5035641260fbd6ba27e6f5d
Jonathan Amsterdam <jba@google.com>
2019-10-21 11:42:51 -0400

    content/static/js: support jump-to-identifier
    On the documentation page, add a dialog box that lets the user jump to
    an identifier's documentation.
    
    Based on the similar feature in gddo, but reimplemented
    to avoid jquery and bootstrap.
    
    Still to be done:
    
    - (b/143456340) Add style to css
    - (b/143454398) Highlight the active element in the box and support up and down arrow keys
    - (b/143456593) Add a polyfill for browsers that don't support <dialog>
    
    Change-Id: Ic89c5bdb7e758f8b55d1b8368a4bc5acd899af8d
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/584342
    CI-Result: Cloud Build <devtools-proctor-result-processor@system.gserviceaccount.com>
    Reviewed-by: Alexander Rakoczy <rakoczy@google.com>

---
+++ content/static/js/jump.js
@@ -0,0 +1,167 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file implements the behavior of the "jump to identifer" dialog for
+// Go package documentation.
+// The DOM for the dialog is at the bottom of content/static/html/pages/pkg_doc.tmpl.
+// The CSS is in content/static/css/stylesheet.css.
+
+// The dialog is activated by pressing the 'f' key. It presents a list
+// (#JumpDialog-list) of all Go identifiers displayed in the documentation.
+// Entering text in the dialog's text box (#JumpDialog-filter) restricts the
+// list to identifiers containing the text. Clicking on an identifier jumps to
+// its documentation.
+
+// This code is based on
+// https://go.googlesource.com/gddo/+/refs/heads/master/gddo-server/assets/site.js.
+// It was modified to remove the dependence on jquery and bootstrap.
+
+const jumpDialog = document.querySelector('.JumpDialog');
+const jumpList = jumpDialog.querySelector('.JumpDialog-list');
+const jumpFilter = jumpDialog.querySelector('.JumpDialog-filter');
+
+let jumpListItems; // All the identifiers in the doc; computed only once.
+
+// collectJumpListItems returns a list of items, one for each identifier in the
+// documentation on the current page.
+function collectJumpListItems() {
+  const items = [];
+  // A map from id to bool, to dedup DOM ids. The doc DOM has duplicate ids (b/143456059).
+  // We assume the first one is the one we want.
+  const seen = {};
+  // Attempt to find the relevant elements by looking through every element in the
+  // .Documentation DOM that has an id attribute of a certain form.
+  // TODO(b/143456714) Put a data-kind attribute on each relevant element, so this would
+  // be more precise.
+  const doc = document.querySelector('.Documentation');
+  for (const el of doc.querySelectorAll('*[id]')) {
+    const id = el.getAttribute('id');
+    if (!seen[id] && /^[^_][^-]*$/.test(id)) {
+      seen[id] = true;
+      items.push(newJumpListItem(el));
+    }
+  }
+  // Clicking on any of the links closes the dialog.
+  for (const item of items) {
+    item.link.addEventListener('click', function() { jumpDialog.close(); });
+  }
+  // Sort case-insensitively by identifier name.
+  items.sort(function (a, b) { return a.lower.localeCompare(b.lower); });
+  return items;
+}
+
+
+// newJumpListItem creates a new item for the DOM element el.
+// An item is an object with:
+// - name: the element's id (which is the identifer name)
+// - kind: the element's kind (function, variable, etc.),
+// - link: a link ('a' tag) to the element
+// - lower: the name in lower case, just for sorting
+function newJumpListItem(el) {
+    const a = document.createElement('a');
+    const name = el.getAttribute('id');
+    a.setAttribute('href', '#' + name);
+    a.setAttribute('tabindex', '-1');
+    return {
+        link: a,
+        name: name,
+        kind: guessKind(el),
+        lower: name.toLowerCase(), // for sorting
+    };
+}
+
+// guessKind tries to guess the kind of el by looking around the DOM.
+// Fixing b/143456714 would make this unnecessary.
+function guessKind(el) {
+  switch (el.getAttribute('class')) {
+    case 'Documentation-functionHeader':
+    case 'Documentation-typeFuncHeader':
+      return 'function';
+    case 'Documentation-typeHeader':
+      return 'type';
+    case 'Documentation-typeMethodHeader':
+      return 'method';
+    default:
+      const sec = el.closest('section');
+      switch (sec.getAttribute('class')) {
+        case 'Documentation-variables':
+          return 'variable';
+        case 'Documentation-constants':
+          return 'constant';
+	case 'Documentation-types':
+          return 'field';
+        default:
+          return '';
+      }
+  }
+}
+
+let lastFilterValue;    // The last contents of the filter text box.
+
+// updateJumpList sets the elements of the dialog list to
+// everything whose name contains filter.
+function updateJumpList(filter) {
+  lastFilterValue = filter;
+  if (!jumpListItems) {
+    jumpListItems = collectJumpListItems();
+  }
+  // Remove all children from list.
+  while (jumpList.firstChild) {
+    jumpList.firstChild.remove();
+  }
+  // Make a regexp corresponding to filter. The result will match any string
+  // containing filter, case-insensitively. Escape the regexp metacharacters in
+  // filter.
+  const re = new RegExp(filter.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1'), 'gi');
+  for (const item of jumpListItems) {
+    var name = item.name;
+    if (filter) {
+      // Boldify the substring of name matching the filter.
+      name = name.replace(re, function (s) { return '<b>' + s + '</b>'; });
+      if (name == item.name) {
+        // We didn't change name, so it didn't match the filter.
+        continue;
+      }
+    }
+    // The text we display includes the name (with filter match bolded), and the
+    // kind in italics.
+    item.link.innerHTML = name + ' <i>' + item.kind + '</i>';
+    jumpList.appendChild(item.link);
+  }
+}
+
+// Pressing a key in the filter updates the list (if the filter actually changed).
+jumpFilter.addEventListener('keyup', function(event) {
+  if (jumpFilter.value.toUpperCase() != lastFilterValue.toUpperCase()) {
+    updateJumpList(jumpFilter.value);
+  }
+});
+
+// Pressing enter in the filter selects the first element in the list.
+// TODO(b/143454398) add arrow keys and track the active list element.
+jumpFilter.addEventListener('keydown', function(event) {
+  const enterKey = 13
+  if (event.which == enterKey) {
+    event.preventDefault();
+    const active = jumpList.firstChild;
+    if (active) active.click();
+  }
+});
+
+// Pressing 'f' anywhere in the document opens the dialog.
+// Ignore 'f' if the dialog is already open, or if it is pressed on a component
+// that wants to consume it.
+document.addEventListener('keypress', function (e) {
+  if (jumpDialog.open) return;
+  const t = e.target.tagName;
+  if (t == 'INPUT' || t == 'SELECT' || t == 'TEXTAREA' ) return;
+  if (e.target.contentEditable && e.target.contentEditable == 'true') return;
+  if (e.metaKey || e.ctrlKey) return;
+  const ch = String.fromCharCode(e.which);
+  if (ch != 'f') return;
+  e.preventDefault();
+  jumpFilter.value = '';
+  updateJumpList('');
+  jumpDialog.showModal();
+});
