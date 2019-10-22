beb50f2366cd95d40a650601f9c4acb6243b31df
Julie Qiu <julie@golang.org>
2019-10-21 23:58:17 -0400

    internal/etl/dochtml: add class names to html
    Class names are added to functions, methods and types in the
    documentation HTML template, so that it is easier to modify CSS classes
    on the frontend.
    
    Updates b/135755729
    
    Change-Id: Ie6ec52a0b527fec37f45e8efadd6e046d9d268b6
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/580175
    Reviewed-by: Alexander Rakoczy <rakoczy@google.com>

--- internal/etl/dochtml/dochtml.go
+++ internal/etl/dochtml/dochtml.go
@@ -227,6 +227,8 @@ func exampleID(id, suffix string) string {
 
 // htmlPackage is the template used to render
 // documentation HTML.
+// TODO(b/142795082): finalize URL scheme and design for notes, then factor out
+// inline CSS style.
 var htmlPackage = template.Must(template.New("package").Funcs(
 	map[string]interface{}{
 		"ternary": func(q, a, b interface{}) interface{} {
@@ -245,142 +247,189 @@ var htmlPackage = template.Must(template.New("package").Funcs(
 	},
 ).Parse(`{{- "" -}}
 {{- if or .Doc .Consts .Vars .Funcs .Types .Examples.List -}}
-	<ul>{{"\n" -}}
+	<ul class="Documentation-toc">{{"\n" -}}
 	{{- if or .Doc (index .Examples.Map "") -}}
-		<li><a href="#pkg-overview">Overview</a></li>{{"\n" -}}
+		<li class="Documentation-tocItem"><a href="#pkg-overview">Overview</a></li>{{"\n" -}}
 	{{- end -}}
 	{{- if or .Consts .Vars .Funcs .Types -}}
-		<li><a href="#pkg-index">Index</a></li>{{"\n" -}}
+		<li class="Documentation-tocItem"><a href="#pkg-index">Index</a></li>{{"\n" -}}
 	{{- end -}}
 	{{- if .Examples.List -}}
-		<li><a href="#pkg-examples">Examples</a></li>{{"\n" -}}
+		<li class="Documentation-tocItem"><a href="#pkg-examples">Examples</a></li>{{"\n" -}}
 	{{- end -}}
 	</ul>{{"\n" -}}
 {{- end -}}
 
 {{- if or .Doc (index .Examples.Map "") -}}
-	<h2 id="pkg-overview">Overview <a href="#pkg-overview">¶</a></h2>{{"\n\n" -}}
-	{{render_doc .Doc}}{{"\n" -}}
-	{{- template "example" (index .Examples.Map "") -}}
+        <section class="Documentation-overview">
+		<h2 id="pkg-overview" class="Documentation-overviewHeader">Overview <a href="#pkg-overview">¶</a></h2>{{"\n\n" -}}
+		{{render_doc .Doc}}{{"\n" -}}
+		{{- template "example" (index .Examples.Map "") -}}
+	</section>
 {{- end -}}
 
 {{- if or .Consts .Vars .Funcs .Types -}}
-	<h2 id="pkg-index">Index <a href="#pkg-index">¶</a></h2>{{"\n\n" -}}
-	<ul>{{"\n" -}}
-	{{- if .Consts -}}<li><a href="#pkg-constants">Constants</a></li>{{"\n"}}{{- end -}}
-	{{- if .Vars -}}<li><a href="#pkg-variables">Variables</a></li>{{"\n"}}{{- end -}}
-	{{- range .Funcs -}}<li><a href="#{{.Name}}">{{render_synopsis .Decl}}</a></li>{{"\n"}}{{- end -}}
-	{{- range .Types -}}
-		{{- $tname := .Name -}}
-		<li><a href="#{{$tname}}">type {{$tname}}</a></li>{{"\n"}}
-		{{- with .Funcs -}}
-			<ul>{{"\n" -}}
-			{{range .}}<li><a href="#{{.Name}}">{{render_synopsis .Decl}}</a></li>{{"\n"}}{{end}}
-			</ul>{{"\n" -}}
-		{{- end -}}
-		{{- with .Methods -}}
-			<ul>{{"\n" -}}
-			{{range .}}<li><a href="#{{$tname}}.{{.Name}}">{{render_synopsis .Decl}}</a></li>{{"\n"}}{{end}}
-			</ul>{{"\n" -}}
-		{{- end -}}
-	{{- end -}}
-	{{- range $marker, $item := .Notes -}}
-		<li><a href="#pkg-note-{{$marker}}">{{$marker}}s</a></li>
-	{{- end -}}
-	</ul>{{"\n" -}}
-	{{- if .Examples.List -}}
-	<h3 id="pkg-examples">Examples <a href="#pkg-examples">¶</a></h3>{{"\n" -}}
-		<ul>{{"\n" -}}
-		{{- range .Examples.List -}}
-			<li><a href="#{{.ID}}">{{or .ParentID "Package"}}{{with .Suffix}} ({{.}}){{end}}</a></li>{{"\n" -}}
-		{{- end -}}
+        <section class="Documentation-index">
+		<h2 id="pkg-index" class="Documentation-indexHeader">Index <a href="#pkg-index">¶</a></h2>{{"\n\n" -}}
+		<ul class="Documentation-indexList">{{"\n" -}}
+			{{- if .Consts -}}<li class="Documentation-indexConstants"><a href="#pkg-constants">Constants</a></li>{{"\n"}}{{- end -}}
+			{{- if .Vars -}}<li class="Documentation-indexVariables"><a href="#pkg-variables">Variables</a></li>{{"\n"}}{{- end -}}
+
+			{{- range .Funcs -}}
+			<li class="Documentation-indexFunction">
+				<a href="#{{.Name}}">{{render_synopsis .Decl}}</a>
+			</li>{{"\n"}}
+			{{- end -}}
+
+			{{- range .Types -}}
+				{{- $tname := .Name -}}
+				<li class="Documentation-indexType"><a href="#{{$tname}}">type {{$tname}}</a></li>{{"\n"}}
+				{{- with .Funcs -}}
+					<ul class="Documentation-indexTypeFunctions">{{"\n" -}}
+					{{range .}}<li><a href="#{{.Name}}">{{render_synopsis .Decl}}</a></li>{{"\n"}}{{end}}
+					</ul>{{"\n" -}}
+				{{- end -}}
+				{{- with .Methods -}}
+					<ul class="Documentation-indexTypeMethods">{{"\n" -}}
+					{{range .}}<li><a href="#{{$tname}}.{{.Name}}">{{render_synopsis .Decl}}</a></li>{{"\n"}}{{end}}
+					</ul>{{"\n" -}}
+				{{- end -}}
+			{{- end -}}
+
+			{{- range $marker, $item := .Notes -}}
+			<li class="Documentation-indexNote"><a href="#pkg-note-{{$marker}}">{{$marker}}s</a></li>
+			{{- end -}}
 		</ul>{{"\n" -}}
+	</section>
+
+	{{- if .Examples.List -}}
+	<section class="Documentation-examples">
+		<h3 id="pkg-examples" class="Documentation-examplesHeader">Examples <a href="#pkg-examples">¶</a></h3>{{"\n" -}}
+		<ul class="Documentation-examplesList">{{"\n" -}}
+			{{- range .Examples.List -}}
+				<li><a href="#{{.ID}}">{{or .ParentID "Package"}}{{with .Suffix}} ({{.}}){{end}}</a></li>{{"\n" -}}
+			{{- end -}}
+		</ul>{{"\n" -}}
+	</section>
 	{{- end -}}
 
-	{{- if .Consts -}}<h3 id="pkg-constants">Constants <a href="#pkg-constants">¶</a></h3>{{"\n"}}{{- end -}}
-	{{- range .Consts -}}
-		{{- $out := render_decl .Doc .Decl -}}
-		{{- $out.Decl -}}
-		{{- $out.Doc -}}
-		{{"\n"}}
-	{{- end -}}
-
-	{{- if .Vars -}}<h3 id="pkg-variables">Variables <a href="#pkg-variables">¶</a></h3>{{"\n"}}{{- end -}}
-	{{- range .Vars -}}
-		{{- $out := render_decl .Doc .Decl -}}
-		{{- $out.Decl -}}
-		{{- $out.Doc -}}
-		{{"\n"}}
-	{{- end -}}
-
-	{{- range .Funcs -}}
-		<h3 id="{{.Name}}">func {{source_link .Name .Decl}} <a href="#{{.Name}}">¶</a></h3>{{"\n"}}
-		{{- $out := render_decl .Doc .Decl -}}
-		{{- $out.Decl -}}
-		{{- $out.Doc -}}
-		{{"\n"}}
-		{{- template "example" (index $.Examples.Map .Name) -}}
-	{{- end -}}
-
-	{{- range .Types -}}
-		{{- $tname := .Name -}}
-		<h3 id="{{.Name}}">type {{source_link .Name .Decl}} <a href="#{{.Name}}">¶</a></h3>{{"\n"}}
-		{{- $out := render_decl .Doc .Decl -}}
-		{{- $out.Decl -}}
-		{{- $out.Doc -}}
-		{{"\n"}}
-		{{- template "example" (index $.Examples.Map .Name) -}}
-
+	{{- if .Consts -}}
+	<section class="Documentation-constants">
+		<h3 id="pkg-constants" class="Documentation-constantsHeader">Constants <a href="#pkg-constants">¶</a></h3>{{"\n"}}
 		{{- range .Consts -}}
 			{{- $out := render_decl .Doc .Decl -}}
 			{{- $out.Decl -}}
 			{{- $out.Doc -}}
 			{{"\n"}}
 		{{- end -}}
+	</section>
+	{{- end -}}
 
+	{{- if .Vars -}}
+	<section class="Documentation-variables">
+		<h3 id="pkg-variables" class="Documentation-variablesHeader">Variables <a href="#pkg-variables">¶</a></h3>{{"\n"}}
 		{{- range .Vars -}}
 			{{- $out := render_decl .Doc .Decl -}}
 			{{- $out.Decl -}}
 			{{- $out.Doc -}}
 			{{"\n"}}
 		{{- end -}}
+	</section>
+	{{- end -}}
 
+	{{- if .Funcs -}}
+	<section class="Documentation-functions">
 		{{- range .Funcs -}}
-			<h3 id="{{.Name}}">func {{source_link .Name .Decl}} <a href="#{{.Name}}">¶</a></h3>{{"\n"}}
+		<div class="Documentation-function">
+			<h3 id="{{.Name}}" class="Documentation-functionHeader">func {{source_link .Name .Decl}} <a href="#{{.Name}}">¶</a></h3>{{"\n"}}
 			{{- $out := render_decl .Doc .Decl -}}
 			{{- $out.Decl -}}
 			{{- $out.Doc -}}
 			{{"\n"}}
 			{{- template "example" (index $.Examples.Map .Name) -}}
+		</div>
 		{{- end -}}
+	</section>
+	{{- end -}}
 
-		{{- range .Methods -}}
-			{{- $name := (printf "%s.%s" $tname .Name) -}}
-			<h3 id="{{$name}}">func ({{.Recv}}) {{source_link .Name .Decl}} <a href="#{{$name}}">¶</a></h3>{{"\n"}}
+	{{- if .Types -}}
+	<section class="Documentation-types">
+		{{- range .Types -}}
+		<div class="Documentation-type">
+			{{- $tname := .Name -}}
+			<h3 id="{{.Name}}" class="Documentation-typeHeader">type {{source_link .Name .Decl}} <a href="#{{.Name}}">¶</a></h3>{{"\n"}}
 			{{- $out := render_decl .Doc .Decl -}}
 			{{- $out.Decl -}}
 			{{- $out.Doc -}}
 			{{"\n"}}
-			{{- template "example" (index $.Examples.Map $name) -}}
+			{{- template "example" (index $.Examples.Map .Name) -}}
+
+			{{- range .Consts -}}
+			<div class="Documentation-typeConstant">
+				{{- $out := render_decl .Doc .Decl -}}
+				{{- $out.Decl -}}
+				{{- $out.Doc -}}
+				{{"\n"}}
+			</div>
+			{{- end -}}
+
+			{{- range .Vars -}}
+			<div class="Documentation-typeVariable">
+				{{- $out := render_decl .Doc .Decl -}}
+				{{- $out.Decl -}}
+				{{- $out.Doc -}}
+				{{"\n"}}
+			</div>
+			{{- end -}}
+
+			{{- range .Funcs -}}
+			<div class="Documentation-typeFunc">
+				<h3 id="{{.Name}}" class="Documentation-typeFuncHeader">func {{source_link .Name .Decl}} <a href="#{{.Name}}">¶</a></h3>{{"\n"}}
+				{{- $out := render_decl .Doc .Decl -}}
+				{{- $out.Decl -}}
+				{{- $out.Doc -}}
+				{{"\n"}}
+				{{- template "example" (index $.Examples.Map .Name) -}}
+			</div>
+			{{- end -}}
+
+			{{- range .Methods -}}
+			<div class="Documentation-typeMethod">
+				{{- $name := (printf "%s.%s" $tname .Name) -}}
+				<h3 id="{{$name}}" class="Documentation-typeMethodHeader">func ({{.Recv}}) {{source_link .Name .Decl}} <a href="#{{$name}}">¶</a></h3>{{"\n"}}
+				{{- $out := render_decl .Doc .Decl -}}
+				{{- $out.Decl -}}
+				{{- $out.Doc -}}
+				{{"\n"}}
+				{{- template "example" (index $.Examples.Map $name) -}}
+			</div>
+			{{- end -}}
+		</div>
 		{{- end -}}
+	</section>
 	{{- end -}}
 {{- end -}}
 
-{{/* TODO(b/142795082): finalize URL scheme and design, then factor out inline CSS style */}}
-{{- range $marker, $content := .Notes -}}
-	<h2 id="pkg-note-{{$marker}}">{{$marker}}s <a href="#pkg-note-{{$marker}}">¶</a></h2>
-	<ul style="padding-left: 20px; list-style: initial;">{{"\n" -}}
-	{{- range $v := $content -}}
-		<li style="margin: 6px 0 6px 0;">{{render_doc $v.Body}}</li>
+{{- if .Notes -}}
+<section class="Documentation-notes">
+	{{- range $marker, $content := .Notes -}}
+	<div class="Documentation-note">
+		<h2 id="pkg-note-{{$marker}}" class="Documentation-noteHeader">{{$marker}}s <a href="#pkg-note-{{$marker}}">¶</a></h2>
+		<ul class="Documentation-noteList" style="padding-left: 20px; list-style: initial;">{{"\n" -}}
+		{{- range $v := $content -}}
+			<li style="margin: 6px 0 6px 0;">{{render_doc $v.Body}}</li>
+		{{- end -}}
+		</ul>{{"\n" -}}
+	</div>
 	{{- end -}}
-	</ul>{{"\n" -}}
+</section>
 {{- end -}}
 
 {{- define "example" -}}
 	{{- range . -}}
-	<details id="{{.ID}}" class="example">{{"\n" -}}
-		<summary class="example-header">Example{{with .Suffix}} ({{.}}){{end}} <a href="#{{.ID}}">¶</a></summary>{{"\n" -}}
-		<div class="example-body">{{"\n" -}}
+	<details id="{{.ID}}" class="Documentation-exampleDetails">{{"\n" -}}
+		<summary class="Documentation-exampleDetailsHeader">Example{{with .Suffix}} ({{.}}){{end}} <a href="#{{.ID}}">¶</a></summary>{{"\n" -}}
+		<div class="Documentation-exampleDetailsBody">{{"\n" -}}
 			{{- if .Doc -}}{{render_doc .Doc}}{{"\n" -}}{{- end -}}
 			<p>Code:</p>{{"\n" -}}
 			{{render_code .Code}}{{"\n" -}}
