2f0a393e47a3b14cde4ad04ac0c125f7aeb8ae6f
Julie Qiu <julie@golang.org>
2019-10-23 00:53:23 -0400

    migrations: fix bug in to_tsv_parent_directories
    Previously, the stored procedure to_tsv_parent_directories did not
    account for stdlib packages. This led to incorrect tsvector strings
    for those packages. This CL fixes that bug and repopulates
    packages.tsv_parent_directories and search_documents.tsv_parent_directories.
    
    Additionally the following functions were previously left out of the
    do wn migration for 000001, and are now added:
    * trigger_modify_packages_tsv_parent_directories
    * trigger_modify_search_documents_tsv_parent_directories
    
    Updates b/142392929
    
    Change-Id: Ib6f393695715a4f743060029413c9c5e44612cc1
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/581195
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- migrations/000001_initial_schema_from_pg_dump.down.sql
+++ migrations/000001_initial_schema_from_pg_dump.down.sql
@@ -3,24 +3,26 @@
 -- license that can be found in the LICENSE file.
 
 DROP TABLE
-	versions,
-	packages,
-	imports,
-	imports_unique,
-	licenses,
-	excluded_prefixes,
-	module_version_states,
-	search_documents;
+    versions,
+    packages,
+    imports,
+    imports_unique,
+    licenses,
+    excluded_prefixes,
+    module_version_states,
+    search_documents;
 
 DROP FUNCTION
-	hll_hash,
-	hll_zeros,
-	popular_search,
-	trigger_modify_updated_at,
-	to_tsvector_parent_directories;
+    hll_hash,
+    hll_zeros,
+    popular_search,
+    trigger_modify_updated_at,
+    trigger_modify_packages_tsv_parent_directories,
+    trigger_modify_search_documents_tsv_parent_directories,
+    to_tsvector_parent_directories;
 
 DROP TYPE
-	version_type,
-	search_result;
+    version_type,
+    search_result;
 
 DROP TEXT SEARCH CONFIGURATION golang;
---
+++ migrations/000002_fix_to_tsv_parent_directories.down.sql
@@ -0,0 +1,37 @@
+-- Copyright 2019 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+--
+-- This schema migration is created from squashing all of our existing
+-- migration files in
+-- 
+BEGIN;
+
+CREATE OR REPLACE FUNCTION to_tsvector_parent_directories(package_path text, module_path text) RETURNS tsvector
+    LANGUAGE plpgsql
+    AS $$
+  DECLARE
+    sub_path TEXT;
+    parent_directories TEXT := module_path;
+    sub_directories TEXT[][];
+    current_directory TEXT := module_path;
+    tsv_parent_directories TSVECTOR := module_path::tsvector;
+  BEGIN
+    IF package_path = module_path THEN
+      RETURN tsv_parent_directories;
+    END IF;
+    -- +2 because substr is one-based and we need to include the trailing slash
+    sub_path := substr(package_path, length(module_path) + 2);
+    sub_directories := regexp_split_to_array(sub_path, '/');
+    FOR i IN 1..cardinality(sub_directories) LOOP
+      current_directory := current_directory || '/' || sub_directories[i];
+      parent_directories = parent_directories || ' ' || current_directory;
+    END LOOP;
+    RETURN parent_directories::tsvector;
+END;
+$$;
+
+UPDATE packages SET tsv_parent_directories = to_tsvector_parent_directories(path, module_path);
+UPDATE search_documents SET tsv_parent_directories = to_tsvector_parent_directories(package_path, module_path);
+
+END;
