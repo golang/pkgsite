5a3ba67fc5a86bd8212a4477962b5ac109f6c42d
Jonathan Amsterdam <jba@google.com>
2019-11-24 10:21:17 -0500

    internal/version: construct sortable version strings
    Add version.ForSorting, which constructs an ASCII string from
    a version that has the correct semver ordering.
    
    With this, we can replace our four semver columns with one,
    and use a "<" operator instead of a more complicated expression.
    
    This encoding also fixes a small bug in the previous one (see
    b/139482696), and removes limits like the 20-character maximum for
    padding a prerelease number, and the requirement that the major, minor
    and patch versions fit into machine integers.
    
    Sometimes we don't follow semver ordering, because we want
    to list release versions before prereleases. We've been doing
    that with an expression like
    
      CASE WHEN prerelease = '~' THEN 0 ELSE 1 END
    
    in the ORDER BY clause. We can instead use
    
     CASE WHEN right(sort_version, 1) = '~' THEN 0 ELSE 1 END
    
    because only release versions end in '~'.
    
    Updates b/139482696.
    
    Change-Id: If24a95cec8ec196ae22fff72fc2bd7fecd79028c
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/605517
    Reviewed-by: Robert Findley <rfindley@google.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -56,3 +56,102 @@ func ParseType(version string) (Type, error) {
 		return TypeRelease, nil
 	}
 }
+
+// ForSorting returns a string that encodes version, so that comparing two such
+// strings follows SemVer precedence, https://semver.org clause 11. It assumes
+// version is valid. The returned string ends in '~' if and only if the version
+// does not have a prerelease.
+//
+// For examples, see TestForSorting.
+func ForSorting(version string) string {
+	bytes := make([]byte, 0, len(version))
+	prerelease := false // we are in the prerelease part
+	nondigit := false   // this part has a non-digit character
+	start := 1          // skip 'v'
+	last := len(version)
+
+	// Add the semver component version[start:end] to the result.
+	addPart := func(end int) {
+		if len(bytes) > 0 {
+			// ',' comes before '-' and all letters and digits, so it correctly
+			// imposes lexicographic ordering on the parts of the version.
+			bytes = append(bytes, ',')
+		}
+		if nondigit {
+			// Prepending the largest printable character '~' to a non-numeric
+			// part, along with the fact that encoded numbers never begin with a
+			// '~', (see appendNumericPrefix), ensures the semver requirement
+			// that numeric identifiers always have lower precedence than
+			// non-numeric ones.
+			bytes = append(bytes, '~')
+		} else {
+			bytes = appendNumericPrefix(bytes, end-start)
+		}
+		bytes = append(bytes, version[start:end]...)
+		start = end + 1 // skip over separator character
+		nondigit = false
+	}
+
+loop:
+	for i, c := range version[start:] {
+		p := i + 1
+		switch {
+		case c == '.': // end of a part
+			addPart(p)
+		case c == '-': // first one is start of prerelease
+			if !prerelease {
+				prerelease = true
+				addPart(p)
+			} else {
+				nondigit = true
+			}
+		case c == '+': // start of build; nothing after this matters
+			last = p
+			break loop
+
+		case c < '0' || c > '9':
+			nondigit = true
+		}
+	}
+	if start < last {
+		addPart(last)
+	}
+	if !prerelease {
+		// Make sure prereleases appear first.
+		bytes = append(bytes, '~')
+	}
+	return string(bytes)
+}
+
+// appendNumericPrefix appends a string representing n to dst.
+// n is the length of a digit string; the value we append is a prefix for the
+// digit string s such that
+//   prefix1 + s1 < prefix2 + s2
+// if and only if the integer denoted by s1 is less than the one denoted by s2.
+// In other words, prefix + s is a string that can be compared with other such
+// strings while preserving the ordering of the numbers.
+//
+// If n==1, there is no prefix. (Single-digit numbers are unchanged.)
+// Otherwise, the prefix is a sequence of lower-case letters encoding n.
+// Examples:
+//   n    prefix
+//   1    <none>
+//   2    a
+//   27   z
+//   28   za
+//   53   zz
+//   54   zza
+// This encoding depends on the ASCII properties that:
+// - digits are ordered numerically
+// - letters are ordered alphabetically
+// - digits order before letters (so "1" < "a10")
+func appendNumericPrefix(dst []byte, n int) []byte {
+	n--
+	for i := 0; i < n/26; i++ {
+		dst = append(dst, 'z')
+	}
+	if rem := n % 26; rem > 0 {
+		dst = append(dst, byte('a'+rem-1))
+	}
+	return dst
+}
---
+++ internal/version/version_test.go
@@ -0,0 +1,100 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package version
+
+import (
+	"testing"
+
+	"golang.org/x/discovery/internal/thirdparty/semver"
+)
+
+func TestForSorting(t *testing.T) {
+	for _, test := range []struct {
+		in, want string
+	}{
+		{"v1.2.3", "1,2,3~"},
+		{"v12.48.301", "a12,a48,b301~"},
+		{"v0.9.3-alpha.1", "0,9,3,~alpha,1"},
+		{"v1.2.3-rc.20150901.-", "1,2,3,~rc,g20150901,~-"},
+		{"v1.2.3-alpha.789+build", "1,2,3,~alpha,b789"},
+	} {
+		got := ForSorting(test.in)
+		if got != test.want {
+			t.Errorf("ForSorting(%s) = %s, want %s", test.in, got, test.want)
+		}
+	}
+}
+
+func TestForSortingOrder(t *testing.T) {
+	// A list of valid semantic versions, in order.
+	semvers := []string{
+		"v0.0.0-20180713131340-b395d2d6f5ee",
+		"v0.0.0-20190124233150-8f7fa2680c82",
+		"v0.0.0",
+		"v0.0.1",
+		"v0.1.0",
+		"v1.0.0-alpha",
+		"v1.0.0-alpha.1",
+		"v1.0.0-alpha.beta",
+		"v1.0.0-beta",
+		"v1.0.0-beta.2",
+		"v1.0.0-beta.11",
+		"v1.0.0-rc.1",
+		"v1.0.0",
+		"v1.2.0",
+		"v1.11.0",
+		"v1.12.0-alph.1",
+		"v1.12.0-alpha",
+		// These next two would order incorrectly if we used '.' as a separator, because
+		// '.' comes after '-'.
+		"v2.0.0-z.a",
+		"v2.0.0-z-",
+		// These next two would sort incorrectly if we did not prepend non-numeric components
+		// with a '~'.
+		"v2.1.0-a.1",
+		"v2.1.0-a.-",
+	}
+
+	// Check that the test has the ordering right according to the semver package.
+	for i := range semvers {
+		if !semver.IsValid(semvers[i]) {
+			t.Fatalf("test is broken: bad semver: %s", semvers[i])
+		}
+		if i > 0 {
+			if semver.Compare(semvers[i-1], semvers[i]) >= 0 {
+				t.Fatalf("test is broken: %s is not less than %s", semvers[i-1], semvers[i])
+			}
+		}
+	}
+
+	// Check that ForSorting produces the correct ordering.
+	var prev string
+	for _, v := range semvers {
+		got := ForSorting(v)
+		if prev != "" && prev >= got {
+			t.Errorf("%s: %s >= %s, want less than", v, prev, got)
+		}
+		prev = got
+	}
+}
+
+func TestAppendNumericPrefix(t *testing.T) {
+	for _, test := range []struct {
+		n    int
+		want string
+	}{
+		{1, ""},
+		{2, "a"},
+		{3, "b"},
+		{26, "y"},
+		{53, "zz"},    // 53-1 = 26*2
+		{100, "zzzu"}, // 100-1 = 26*3 + 21
+	} {
+		got := string(appendNumericPrefix(nil, test.n))
+		if got != test.want {
+			t.Errorf("%d: got %s, want %s", test.n, got, test.want)
+		}
+	}
+}
