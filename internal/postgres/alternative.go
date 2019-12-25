6403e13e11b56b6fc776c83a400e9297cc3419c5
Julie Qiu <julie@golang.org>
2019-12-25 15:21:50 -0500

    postgres: read/write from alternative_module_paths
    The following postgres functions are added to read/write from
    alternative_module_paths:
    
    * InsertAlternativeModulePath: inserts an alternative module path into
      the alternative_module_paths table
    * DeletePackagesWithPrefixes: deletes all packages from the packages
      table with a module path in the alternative_module_paths table
    * IsCanonicalPath: reports whether a path represents the canonical path
      for a package, module or directory
    
    Updates b/131903657
    Updates b/133176189
    Updates b/139364190
    
    Change-Id: I81adf78cde0ec18d56704db245161ad866d78404
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/626662
    CI-Result: Cloud Build <devtools-proctor-result-processor@system.gserviceaccount.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ internal/postgres/alternative.go
@@ -0,0 +1,61 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package postgres
+
+import (
+	"context"
+
+	"golang.org/x/discovery/internal"
+	"golang.org/x/discovery/internal/derrors"
+)
+
+// InsertAlternativeModulePath inserts the alternative module path into the alternative_module_paths table.
+func (db *DB) InsertAlternativeModulePath(ctx context.Context, alternative *internal.AlternativeModulePath) (err error) {
+	derrors.Wrap(&err, "DB.InsertAlternativeModulePath(ctx, %v)", alternative)
+	_, err = db.db.Exec(ctx, `
+		INSERT INTO alternative_module_paths (alternative, canonical)
+		VALUES($1, $2) ON CONFLICT DO NOTHING;`,
+		alternative.Alternative, alternative.Canonical)
+	return err
+}
+
+// DeleteAlternatives deletes all modules with the given path.
+func (db *DB) DeleteAlternatives(ctx context.Context, alternativePath string) (err error) {
+	derrors.Wrap(&err, "DB.DeleteAlternatives(ctx)")
+	_, err = db.db.Exec(ctx, `DELETE FROM versions WHERE module_path = $1;`, alternativePath)
+	return err
+}
+
+// IsAlternativePath reports whether the path represents the canonical path for a
+// package, module or directory.
+func (db *DB) IsAlternativePath(ctx context.Context, path string) (_ bool, err error) {
+	defer derrors.Wrap(&err, "IsAlternativePath(ctx, %q)", path)
+	query := `
+		SELECT EXISTS(
+			SELECT 1
+			FROM alternative_module_paths
+			WHERE $1 LIKE alternative || '%'
+		);`
+	row := db.db.QueryRow(ctx, query, path)
+	var isAlternative bool
+	err = row.Scan(&isAlternative)
+	return isAlternative, err
+}
+
+func (db *DB) getAlternativeModulePath(ctx context.Context, alternativePath string) (_ *internal.AlternativeModulePath, err error) {
+	defer derrors.Wrap(&err, "GetAlternativeModulePath(ctx, %q)", alternativePath)
+	query := `
+		SELECT alternative, canonical
+		FROM alternative_module_paths
+		WHERE alternative = $1;`
+	row := db.db.QueryRow(ctx, query, alternativePath)
+	var alternative, canonical string
+	if err := row.Scan(&alternative, &canonical); err != nil {
+		return nil, err
+	}
+	return &internal.AlternativeModulePath{
+		Alternative: alternative,
+		Canonical:   canonical,
+	}, nil
+}
---
+++ internal/postgres/alternative_test.go
@@ -0,0 +1,128 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package postgres
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/discovery/internal"
+	"golang.org/x/discovery/internal/derrors"
+	"golang.org/x/discovery/internal/testing/sample"
+)
+
+func TestIsAlternativePath(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+	defer ResetTestDB(testDB, t)
+
+	alternativePaths := []*internal.AlternativeModulePath{
+		{Canonical: "rsc.io/quote", Alternative: "github.com/rsc.io/quote"},
+		{Canonical: "gocloud.dev", Alternative: "github.com/google/go-cloud"},
+	}
+	for _, p := range alternativePaths {
+		if err := testDB.InsertAlternativeModulePath(ctx, p); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	pkgData := []struct {
+		pkgPath, modulePath string
+		isAlternative       bool
+	}{
+		{"rsc.io/quote", "rsc.io/quote", false},
+		{"github.com/rsc.io/quote", "github.com/rsc.io/quote", true},
+		{"gocloud.dev/blob/s3blob", "gocloud.dev", false},
+		{"github.com/google/go-cloud/blob/aws", "github.com/google/go-cloud", true},
+		{"github.com/foo/bar", "github.com/foo", false},
+	}
+	for _, data := range pkgData {
+		t.Run(data.pkgPath, func(t *testing.T) {
+			got, err := testDB.IsAlternativePath(ctx, data.pkgPath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got != data.isAlternative {
+				t.Errorf("testDB.IsAlternative(%q) = %t; want = %t", data.pkgPath, got, data.isAlternative)
+			}
+		})
+	}
+
+}
+
+func TestInsertAndDeleteAlternatives(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+	defer ResetTestDB(testDB, t)
+
+	alternativePaths := []*internal.AlternativeModulePath{
+		{Canonical: "rsc.io/quote", Alternative: "github.com/rsc.io/quote"},
+		{Canonical: "gocloud.dev", Alternative: "github.com/google/go-cloud"},
+	}
+	for _, p := range alternativePaths {
+		if err := testDB.InsertAlternativeModulePath(ctx, p); err != nil {
+			t.Fatal(err)
+		}
+		got, err := testDB.getAlternativeModulePath(ctx, p.Alternative)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if diff := cmp.Diff(got, p); diff != "" {
+			t.Fatalf("mismatch (-want +got):\n%s", diff)
+		}
+	}
+
+	pkgData := []struct {
+		pkgPath, modulePath string
+		isAlternative       bool
+	}{
+		{"rsc.io/quote", "rsc.io/quote", false},
+		{"github.com/rsc.io/quote", "github.com/rsc.io/quote", true},
+		{"gocloud.dev/blob/s3blob", "gocloud.dev", false},
+		{"github.com/google/go-cloud/blob/aws", "github.com/google/go-cloud", true},
+		{"github.com/foo/bar", "github.com/foo", false},
+	}
+	for _, data := range pkgData {
+		v := sample.Version()
+		v.ModulePath = data.modulePath
+		p := sample.Package()
+		p.Path = data.pkgPath
+		v.Packages = []*internal.Package{p}
+		if err := testDB.InsertVersion(ctx, v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, data := range pkgData {
+		t.Run("GetPackage-"+data.pkgPath, func(t *testing.T) {
+			isAlternative, err := testDB.IsAlternativePath(ctx, data.pkgPath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if isAlternative {
+				if err := testDB.DeleteAlternatives(ctx, data.modulePath); err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			got, err := testDB.GetPackage(ctx, data.pkgPath, data.modulePath, internal.LatestVersion)
+			if data.isAlternative {
+				if !errors.Is(err, derrors.NotFound) {
+					t.Errorf("Expected pkg %q to be deleted because it is does not have a canonical path", data.modulePath)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if got.Path != data.pkgPath || got.ModulePath != data.modulePath {
+				t.Errorf("testDB.GetPackage(%q, latest) = %q, %q; want = %q, %q)", data.pkgPath, got.Path, got.ModulePath, data.pkgPath, data.modulePath)
+			}
+		})
+	}
+}
