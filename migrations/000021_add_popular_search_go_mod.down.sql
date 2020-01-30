df4b3f6165d22e28f127715a82bbd358ec3fa819
Jonathan Amsterdam <jba@google.com>
2020-01-30 14:35:15 -0500

    migrations: add popular_search_go_mod stored procedure
    Add a stored procedure that is identical to popular_search,
    but takes the has_go_mod field into account.
    
    Updates b/148519337.
    
    Change-Id: I3b67bc56f3d3f61b8d8d9e356859d8bbdc212aaf
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/653244
    CI-Result: Cloud Build <devtools-proctor-result-processor@system.gserviceaccount.com>
    Reviewed-by: Julie Qiu <julieqiu@google.com>

---
+++ migrations/000021_add_popular_search_go_mod.down.sql
@@ -0,0 +1,9 @@
+-- Copyright 2020 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+DROP FUNCTION popular_search_go_mod;
+
+END;
