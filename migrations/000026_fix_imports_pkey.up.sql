081889f054fd89e922a7079e4ae1543fdb57087c
Jonathan Amsterdam <jba@google.com>
2020-02-18 18:50:53 -0500

    migrations: fix the imports table primary key
    The primary key does not include from_version. That means that a
    package that lives in two modules at the same version
    (e.g. github.com/hashicorp/consul/api at v1.4.0) will not be correctly
    represented in the table: only one of the modules will be present for
    a given to_path.
    
    Fix by adding from_module_path to the primary key.
    
    Note: despite the presence of a "down" file, this migration cannot be
    undone, because new rows can violate the old primary key constraint.
    
    Change-Id: Ifcf40dc8a9c41108e676df4c23c3f7786477b861
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/670387
    Reviewed-by: Julie Qiu <julieqiu@google.com>
    CI-Result: Cloud Build <devtools-proctor-result-processor@system.gserviceaccount.com>

---
+++ migrations/000026_fix_imports_pkey.up.sql
@@ -0,0 +1,10 @@
+-- Copyright 2020 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE imports DROP CONSTRAINT imports_pkey;
+ALTER TABLE imports ADD PRIMARY KEY (to_path, from_path, from_version, from_module_path);
+
+END;
