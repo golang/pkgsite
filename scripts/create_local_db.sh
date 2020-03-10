811c859307ac314d48c644a71a38f2a4e65a9a95
Julie Qiu <julie@golang.org>
2020-03-10 16:08:34 -0400

    scripts: add create_local_db.sh
    A script is added to create a database locally with the following
    settings:
    
    OWNER = postgres
      https://www.postgresql.org/docs/9.1/manage-ag-createdb.html
    
    TEMPLATE = template0
      template0 is a standard databse system that contains only the standard
      objects predefined by your version of PostgreSQL. It contains none of
      the site-local additions in template1. This is particularly handy when
      restoring a pg_dump dump: the dump script should be restored in a virgin
      database to ensure that one recreates the correct contents of the dumped
      database, without conflicting with objects that might have been added to
      template1 later on.
      It is also necessary because attempting to create the database with
      template1 gives us the follow error:
      ERROR:  new collation (C) is
      incompatible with the collation of the template database (en_US.UTF-8)
      HINT:  Use the same collation as in the template database, or use
      template0 as template.
      https://www.postgresql.org/docs/11/manage-ag-templatedbs.html
    
    LC_COLLATE = 'C'
    command   Sets the string sort order so that uppercase always comes before lowercase.
      https://www.postgresql.org/docs/11/collation.html
    
    LC_CTYPE = 'C'
      Sets the character classification (What is a letter? Its upper-case equivalent?)
      https://www.postgresql.org/docs/current/locale.html
    
    Change-Id: I19584ff2ff3d23e43314d0d9056e704cbf636df0
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/687555
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ scripts/create_local_db.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S bash -e
+
+# Copyright 2019 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# Script for creating a new database locally.
+
+source devtools/lib.sh || { echo "Are you at repo root?"; exit 1; }
+
+echo "CREATE DATABASE \"discovery-database\" \
+	OWNER = postgres \
+	TEMPLATE=template0 \
+	LC_COLLATE = 'C' \
+	LC_CTYPE = 'C';" | psql 'host=127.0.0.1 sslmode=disable user=postgres'
