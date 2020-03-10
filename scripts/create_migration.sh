605781fa0cc70069d4c3c4ec3f0498a0324f8a9c
Julie Qiu <julie@golang.org>
2020-03-10 16:08:46 -0400

    scripts: add create_migration.sh
    A script is added to create a new migration file.
    
    Change-Id: I851b59a779ce1201d214e515d39d3a2922173ba0
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/687556
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ scripts/create_migration.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S bash -e
+
+# Copyright 2019 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# Script for creating a new migration file.
+
+migrate create -ext sql -dir migrations -seq $1
+HEADER="-- Copyright 2020 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+-- Write your migration here.
+
+END;"
+for f in $(ls migrations | tail -n 2); do echo "$HEADER" >> "migrations/$f"; done
