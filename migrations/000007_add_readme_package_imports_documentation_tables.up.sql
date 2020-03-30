106e619e2fdfb9fd2cc8a984d563e336c0e83582
Julie <julie@golang.org>
2020-03-30 13:35:47 -0400

    migrations: add readme, documentation and package_imports tables
    The following tables are added:
    - readme
    - documentation
    - package_imports
    
    These are based on the new paths centered data model.
    
    Change-Id: Ic9bdd3e34bedbbaa41260c305afb00a9d506a9d6
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/704778
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000007_add_readme_package_imports_documentation_tables.up.sql
@@ -0,0 +1,37 @@
+-- Copyright 2020 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+CREATE TABLE readmes (
+    path_id INTEGER NOT NULL PRIMARY KEY REFERENCES paths(id) ON DELETE CASCADE,
+    filename text NOT NULL,
+    contents text NOT NULL
+);
+COMMENT ON TABLE readmes IS
+'TABLE readmes contains README files at a given path.';
+
+CREATE TABLE documentation (
+    path_id INTEGER NOT NULL REFERENCES paths(id) ON DELETE CASCADE,
+    goos text NOT NULL,
+    goarch text NOT NULL,
+    synopsis text NOT NULL,
+    html text NOT NULL
+    PRIMARY KEY (path_id, goos, goarch)
+);
+COMMENT ON TABLE documentation IS
+'TABLE documentation contains documentation for packages in the database.';
+
+CREATE TABLE package_imports (
+    path_id INTEGER NOT NULL REFERENCES paths(id) ON DELETE CASCADE,
+    to_path text NOT NULL,
+    PRIMARY KEY (path_id, to_path)
+);
+CREATE INDEX idx_package_imports_to_path ON package_imports USING btree (to_path);
+COMMENT ON TABLE package_imports IS
+'TABLE package_imports contains the imports for a package in the paths table. The package represented by path_id imports to_path. We do not store the version and module at which to_path is imported because it is hard to compute.
+
+This table will be renamed to imports, once the current imports table has been deprecated.';
+
+END;
