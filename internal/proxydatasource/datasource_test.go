d7cda53db1a60c90a77142939a7826aaa18ae1e8
Andrew Bonventre <andybons@golang.org>
2020-05-12 14:26:00 -0400

    internal/proxydatasource: increase context timeout in tests
    The current timeout value (1 second) is too short and causes the
    test to fail sporadically.
    
    Fixes b/156383731
    
    Change-Id: I36af99957fb3310c29c84433014346209c2c20c6
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/745554
    CI-Result: Cloud Build <devtools-proctor-result-processor@system.gserviceaccount.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- internal/proxydatasource/datasource_test.go
+++ internal/proxydatasource/datasource_test.go
@@ -39,7 +39,7 @@ func setup(t *testing.T) (context.Context, *DataSource, func()) {
 		},
 	}
 	client, teardownProxy := proxy.SetupTestProxy(t, testModules)
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	return ctx, New(client), func() {
 		teardownProxy()
 		cancel()
