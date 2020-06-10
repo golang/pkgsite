f3201219983099f169ee794b2ed74b8f4d71396f
Jonathan Amsterdam <jba@google.com>
2020-06-10 15:05:13 -0400

    internal/frontend: fix tab display name
    LegacyPackages -> Packages
    
    Change-Id: Idb95758c3e0b44c6756ea1e06423dc4c20ff1873
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/discovery/+/766371
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- internal/frontend/tabs.go
+++ internal/frontend/tabs.go
@@ -93,7 +93,7 @@ var (
 		{
 			Name:              "packages",
 			AlwaysShowDetails: true,
-			DisplayName:       "LegacyPackages",
+			DisplayName:       "Packages",
 			TemplateName:      "subdirectories.tmpl",
 		},
 		{
