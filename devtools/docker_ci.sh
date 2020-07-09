1a979796ae0272aea8ad6f1dafa34a6b231b2caa
Rob Findley <rfindley@google.com>
2020-07-09 11:38:53 -0400

    devtools: use an auto-generated name for ci
    There's no reason to use a hard-coded name when running postgres in the
    CI script (and in fact, that's why we're storing the resulting container
    name in a variable).
    
    Change-Id: I9db408fac8c02f07d038240afed5d17ead9bbefe
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/241738
    Reviewed-by: Julie Qiu <julie@golang.org>

--- devtools/docker_ci.sh
+++ devtools/docker_ci.sh
@@ -36,7 +36,7 @@ script_dir=$(dirname "$(readlink -f "$0")")
 pkgsite_dir=$(readlink -f "${script_dir}/..")
 
 # Run postgres.
-pg_container=$(${maybe_sudo}docker run --rm -d --name pg_pkgsite_ci -e LANG=C postgres:11.4)
+pg_container=$(${maybe_sudo}docker run --rm -d -e LANG=C postgres:11.4)
 trap "${maybe_sudo} docker stop ${pg_container}" EXIT
 
 # Run all.bash. To avoid any port conflict, run in the postgres network.
