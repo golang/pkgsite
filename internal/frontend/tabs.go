b197f4f04fe9329b390b188b0ff4a78fcce16d63
Julie Qiu <julie@golang.org>
2020-08-25 10:28:40 -0400

    internal/frontend: move legacy tab code to legacy_tabs.go
    Legacy code in the tabs.go file is moved to legacy_tabs.go, to make it
    easier to keep track of code in that file and improve readability.
    
    There are no code changes in this CL, only movement.
    
    For golang/go#39629
    
    Change-Id: I1578b6f4b9a6daea5320f1e90805931ad7225636
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/250461
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- internal/frontend/tabs.go
+++ internal/frontend/tabs.go
@@ -12,7 +12,6 @@ import (
 
 	"golang.org/x/pkgsite/internal"
 	"golang.org/x/pkgsite/internal/licenses"
-	"golang.org/x/pkgsite/internal/postgres"
 )
 
 // TabSettings defines tab-specific metadata.
@@ -215,54 +214,6 @@ func fetchDetailsForDirectory(r *http.Request, tab string, ds internal.DataSourc
 		return fetchDirectoryDetails(ctx, ds, vdir, false)
 	case tabLicenses:
 		return fetchLicensesDetails(ctx, ds, vdir.Path, vdir.ModulePath, vdir.Version)
-	}
-	return nil, fmt.Errorf("BUG: unable to fetch details: unknown tab %q", tab)
-}
-
-// legacyFetchDetailsForDirectory returns tab details by delegating to the correct
-// detail handler.
-func legacyFetchDetailsForDirectory(r *http.Request, tab string, dir *internal.LegacyDirectory, licenses []*licenses.License) (interface{}, error) {
-	switch tab {
-	case tabOverview:
-		readme := &internal.Readme{Filepath: dir.LegacyReadmeFilePath, Contents: dir.LegacyReadmeContents}
-		return constructOverviewDetails(r.Context(), &dir.ModuleInfo, readme, dir.LegacyModuleInfo.IsRedistributable, urlIsVersioned(r.URL))
-	case tabSubdirectories:
-		// Ideally we would just use fetchDirectoryDetails here so that it
-		// follows the same code path as fetchDetailsForModule and
-		// fetchDetailsForPackage. However, since we already have the directory
-		// and licenses info, it doesn't make sense to call
-		// postgres.GetDirectory again.
-		return legacyCreateDirectory(dir, licensesToMetadatas(licenses), false)
-	case tabLicenses:
-		return &LicensesDetails{Licenses: transformLicenses(dir.ModulePath, dir.Version, licenses)}, nil
-	}
-	return nil, fmt.Errorf("BUG: unable to fetch details: unknown tab %q", tab)
-}
-
-// legacyFetchDetailsForPackage returns tab details by delegating to the correct detail
-// handler.
-func legacyFetchDetailsForPackage(r *http.Request, tab string, ds internal.DataSource, pkg *internal.LegacyVersionedPackage) (interface{}, error) {
-	ctx := r.Context()
-	switch tab {
-	case tabDoc:
-		return legacyFetchDocumentationDetails(pkg), nil
-	case tabVersions:
-		return legacyFetchPackageVersionsDetails(ctx, ds, pkg.Path, pkg.V1Path, pkg.ModulePath)
-	case tabSubdirectories:
-		return legacyFetchDirectoryDetails(ctx, ds, pkg.Path, &pkg.ModuleInfo, pkg.Licenses, false)
-	case tabImports:
-		return fetchImportsDetails(ctx, ds, pkg.Path, pkg.ModulePath, pkg.Version)
-	case tabImportedBy:
-		db, ok := ds.(*postgres.DB)
-		if !ok {
-			// The proxydatasource does not support the imported by page.
-			return nil, proxydatasourceNotSupportedErr()
-		}
-		return fetchImportedByDetails(ctx, db, pkg.Path, pkg.ModulePath)
-	case tabLicenses:
-		return legacyFetchPackageLicensesDetails(ctx, ds, pkg.Path, pkg.ModulePath, pkg.Version)
-	case tabOverview:
-		return legacyFetchPackageOverviewDetails(ctx, pkg, urlIsVersioned(r.URL))
 	}
 	return nil, fmt.Errorf("BUG: unable to fetch details: unknown tab %q", tab)
 }
