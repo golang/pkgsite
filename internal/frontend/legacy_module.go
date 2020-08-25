76eabf1a5ae400fd92b5c09c0333d0a31ce61d53
Julie Qiu <julie@golang.org>
2020-08-25 10:41:56 -0400

    internal/frontend: add legacyServeModulePageWithModule
    legacyServeModulePageWithModule is added, so that we can modify
    serveModulePageWithModule in future CLs without worrying about
    compatibility with the legacy code.
    
    At the moment, legacyServeModulePageWithModule is just a copy of
    serveModulePageWithModule.
    
    For golang/go#39629
    
    Change-Id: I0ebd5ad510bcca810f8028992a0e8a15cead8f8d
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/250465
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>

--- internal/frontend/legacy_module.go
+++ internal/frontend/legacy_module.go
@@ -5,12 +5,15 @@
 package frontend
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"golang.org/x/pkgsite/internal"
 	"golang.org/x/pkgsite/internal/derrors"
 	"golang.org/x/pkgsite/internal/log"
+	"golang.org/x/pkgsite/internal/stdlib"
 )
 
 // legacyServeModulePage serves details pages for the module specified by modulePath
@@ -30,7 +33,7 @@ func (s *Server) legacyServeModulePage(w http.ResponseWriter, r *http.Request, d
 	mi, err := ds.LegacyGetModuleInfo(ctx, modulePath, resolvedVersion)
 	if err == nil {
 		readme := &internal.Readme{Filepath: mi.LegacyReadmeFilePath, Contents: mi.LegacyReadmeContents}
-		return s.serveModulePage(ctx, w, r, ds, &mi.ModuleInfo, readme, requestedVersion)
+		return s.legacyServeModulePageWithModule(ctx, w, r, ds, &mi.ModuleInfo, readme, requestedVersion)
 	}
 	if !errors.Is(err, derrors.NotFound) {
 		return err
@@ -45,4 +48,50 @@ func (s *Server) legacyServeModulePage(w http.ResponseWriter, r *http.Request, d
 		}
 	}
 	return pathNotFoundError(ctx, "module", modulePath, requestedVersion)
+}
+
+func (s *Server) legacyServeModulePageWithModule(ctx context.Context, w http.ResponseWriter, r *http.Request, ds internal.DataSource,
+	mi *internal.ModuleInfo, readme *internal.Readme, requestedVersion string) error {
+	licenses, err := ds.GetLicenses(ctx, mi.ModulePath, mi.ModulePath, mi.Version)
+	if err != nil {
+		return err
+	}
+	modHeader := createModule(mi, licensesToMetadatas(licenses), requestedVersion == internal.LatestVersion)
+	tab := r.FormValue("tab")
+	settings, ok := moduleTabLookup[tab]
+	if !ok {
+		tab = tabOverview
+		settings = moduleTabLookup[tabOverview]
+	}
+	canShowDetails := modHeader.IsRedistributable || settings.AlwaysShowDetails
+	var details interface{}
+	if canShowDetails {
+		var err error
+		details, err = fetchDetailsForModule(r, tab, ds, mi, licenses, readme)
+		if err != nil {
+			return fmt.Errorf("error fetching page for %q: %v", tab, err)
+		}
+	}
+	pageType := pageTypeModule
+	if mi.ModulePath == stdlib.ModulePath {
+		pageType = pageTypeStdLib
+	}
+
+	page := &DetailsPage{
+		basePage:       s.newBasePage(r, moduleHTMLTitle(mi.ModulePath)),
+		Name:           mi.ModulePath,
+		Settings:       settings,
+		Header:         modHeader,
+		Breadcrumb:     breadcrumbPath(modHeader.ModulePath, modHeader.ModulePath, modHeader.LinkVersion),
+		Details:        details,
+		CanShowDetails: canShowDetails,
+		Tabs:           moduleTabSettings,
+		PageType:       pageType,
+		CanonicalURLPath: constructModuleURL(
+			mi.ModulePath,
+			linkVersion(mi.Version, mi.ModulePath),
+		),
+	}
+	s.servePage(ctx, w, settings.TemplateName, page)
+	return nil
 }
