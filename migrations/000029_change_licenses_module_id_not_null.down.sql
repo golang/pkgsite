05b1c7821d3571c0ab1a835618db7b1e559c6b18
Jonathan Amsterdam <jba@google.com>
2020-09-14 08:10:46 -0400

    migrations: make licenses.module_id NOT NULL
    Change-Id: I268653bfeb05836d6e72c7ad22ef8e1494d8db93
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/254677
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

---
+++ migrations/000029_change_licenses_module_id_not_null.down.sql
@@ -0,0 +1,9 @@
+-- Copyright 2020 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE licenses ALTER COLUMN module_id DROP NOT NULL;
+
+END;
