52e372889d27daaa7aceb7238c7a3f909c37aa58
Jamal Carvalho <jamal@golang.org>
2020-09-18 14:14:11 -0400

    internal/fetch: add separate identifier for mobile sidenav
    Adds an identifier for the mobile sidenav. This will be
    used in the godoc package to separate the html for the
    mobile sidenav from the documentation html.
    
    Change-Id: I4ff1d418286058a4ce2eefa298ace384a8eddc60
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/255940
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- internal/fetch/dochtml/template_sidenav.go
+++ internal/fetch/dochtml/template_sidenav.go
@@ -5,8 +5,9 @@
 package dochtml
 
 const (
-	IdentifierSidenavStart = `<nav class="DocNav js-sideNav">`
-	IdentifierSidenavEnd   = `</nav>`
+	IdentifierSidenavStart       = `<nav class="DocNav js-sideNav">`
+	IdentifierSidenavMobileStart = `<nav class="DocNavMobile js-mobileNav">`
+	IdentifierSidenavEnd         = `</nav>`
 )
 
 const tmplSidenav = `
@@ -99,7 +100,7 @@ const tmplSidenav = `
 			{{end}}
 		</ul>
 	</nav>
-	<nav class="DocNavMobile js-mobileNav">
+	` + IdentifierSidenavMobileStart + `
 		<label for="DocNavMobile-select" class="DocNavMobile-label">
 			<svg class="DocNavMobile-selectIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px">
 				<path d="M0 0h24v24H0z" fill="none"/><path d="M3 9h14V7H3v2zm0 4h14v-2H3v2zm0 4h14v-2H3v2zm16 0h2v-2h-2v2zm0-10v2h2V7h-2zm0 6h2v-2h-2v2z"/>
