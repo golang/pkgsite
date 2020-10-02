8240715769476efb0be6ad6b3acddb4c90a237c0
Julie Qiu <julie@golang.org>
2020-10-01 23:52:38 -0400

    migrations: drop search_documents_package_path_fkey
    The search_documents_package_path_fkey FK is dropped.
    
    For golang/go#39629
    
    Change-Id: I93da9209475c6a17fb5d137b99192d78d4a55b81
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/259009
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000032_drop_search_documents_package_path_fkey.up.sql
@@ -0,0 +1,9 @@
+-- Copyright 2020 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE search_documents DROP CONSTRAINT search_documents_package_path_fkey;
+
+END;
