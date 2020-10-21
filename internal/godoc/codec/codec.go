da6ceb7af4dc7ec1d89fbc3a35020ffdae454889
Jonathan Amsterdam <jba@google.com>
2020-10-21 07:39:52 -0400

    internal/godoc/codec: encoding unsigned integers
    This CL introduces the encoding for Go values, and shows how
    unsigned integers are encoded.
    
    The details are explained in the code comments.
    
    There is no test for EncodeUint in this CL; it needs DecodeUint, in
    the next CL.
    
    Change-Id: I0ceec6d68fa1a7e2bcd9c85196b2da75e49e154e
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/264118
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>

--- internal/godoc/codec/codec.go
+++ internal/godoc/codec/codec.go
@@ -10,6 +10,11 @@
 // go/ast package, which is its sole purpose.
 package codec
 
+import (
+	"encoding/binary"
+	"math"
+)
+
 // An Encoder encodes Go values into a sequence of bytes.
 type Encoder struct {
 	buf []byte
@@ -71,4 +76,80 @@ func (e *Encoder) writeString(s string) {
 
 func (d *Decoder) readString(len int) string {
 	return string(d.readBytes(len))
+}
+
+func (e *Encoder) writeUint32(u uint32) {
+	var buf [4]byte
+	binary.LittleEndian.PutUint32(buf[:], u)
+	e.writeBytes(buf[:])
+}
+
+func (d *Decoder) readUint32() uint32 {
+	return binary.LittleEndian.Uint32(d.readBytes(4))
+}
+
+func (e *Encoder) writeUint64(u uint64) {
+	var buf [8]byte
+	binary.LittleEndian.PutUint64(buf[:], u)
+	e.writeBytes(buf[:])
+}
+
+func (d *Decoder) readUint64() uint64 {
+	return binary.LittleEndian.Uint64(d.readBytes(8))
+}
+
+//////////////// Encoding Scheme
+
+// Every encoded value begins with a single byte that describes what (if
+// anything) follows. There is enough information to skip over the value, since
+// the decoder must be able to do that if it encounters a struct field it
+// doesn't know.
+//
+// Most of the values of that initial byte can be devoted to small unsigned
+// integers. For example, the number 17 is represented by the single byte 17.
+// Only five byte values have special meaning.
+//
+// nBytes (255) indicates that an unsigned integer N is encoded next,
+// followed by N bytes of data. This is used to represent strings and byte
+// slices, as well numbers bigger than can fit into the initial byte. For example,
+// the string "hi" is represented as:
+//   nBytes 2 'h' 'i'
+//
+// Unsigned integers that can't fit into the intial byte are encoded as byte
+// sequences of length 4 or 8, holding little-endian uint32 or uint64 values. We
+// use uint32s where possible to save space. We could have saved more space by
+// also considering 16-byte numbers, or using a variable-length encoding like
+// varints or gob's representation, but it didn't seem worth the additional
+// complexity.
+//
+// TODO: describe nValues, ref, start and end in later CLs.
+
+const (
+	nBytesCode  = 255 - iota // uint n follows, then n bytes
+	nValuesCode              // uint n follows, then n values
+	refCode                  // uint n follows, referring to a previous value
+	startCode                // start of a value of indeterminate length
+	endCode                  // end of a value that began with with start
+	// Bytes less than endCode represent themselves.
+)
+
+// EncodeUint encodes a uint64.
+func (e *Encoder) EncodeUint(u uint64) {
+	switch {
+	case u < endCode:
+		// u fits into the initial byte.
+		e.writeByte(byte(u))
+	case u <= math.MaxUint32:
+		// Encode as a sequence of 4 bytes, the little-endian representation of
+		// a uint32.
+		e.writeByte(nBytesCode)
+		e.writeByte(4)
+		e.writeUint32(uint32(u))
+	default:
+		// Encode as a sequence of 8 bytes, the little-endian representation of
+		// a uint64.
+		e.writeByte(nBytesCode)
+		e.writeByte(8)
+		e.writeUint64(u)
+	}
 }
--- internal/godoc/codec/codec_test.go
+++ internal/godoc/codec/codec_test.go
@@ -5,6 +5,7 @@
 package codec
 
 import (
+	"math"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -12,15 +13,19 @@ import (
 
 func TestLowLevelIO(t *testing.T) {
 	var (
-		b  byte   = 15
-		bs []byte = []byte{4, 10, 8}
-		s         = "hello"
+		b   byte   = 15
+		bs  []byte = []byte{4, 10, 8}
+		s          = "hello"
+		u32 uint32 = 999
+		u64 uint64 = math.MaxUint32 + 1
 	)
 
 	e := NewEncoder()
 	e.writeByte(b)
 	e.writeBytes(bs)
 	e.writeString(s)
+	e.writeUint32(u32)
+	e.writeUint64(u64)
 
 	d := NewDecoder(e.Bytes())
 	if got := d.readByte(); got != b {
@@ -31,5 +36,11 @@ func TestLowLevelIO(t *testing.T) {
 	}
 	if got := d.readString(len(s)); got != s {
 		t.Fatalf("got %q, want %q", got, s)
+	}
+	if got := d.readUint32(); got != u32 {
+		t.Errorf("got %d, want %d", got, u32)
+	}
+	if got := d.readUint64(); got != u64 {
+		t.Errorf("got %d, want %d", got, u64)
 	}
 }
