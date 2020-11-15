85b1a8a32b0e43cc6ccf33a4bff5985888b15983
Julie Qiu <julie@golang.org>
2020-11-15 17:32:58 -0500

    internal/frontend: move code to urlinfo.go
    Code related to parsing the url info for a unit page request is moved
    into urlinfo.go. Pure code in motion.
    
    Change-Id: Ibee62cf8477c73a823c6f9b3f77233b07a17701d
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/270257
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Trust: Julie Qiu <julie@golang.org>

--- internal/frontend/details.go
+++ internal/frontend/details.go
@@ -9,18 +9,15 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/google/safehtml/template"
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/tag"
-	"golang.org/x/mod/module"
 	"golang.org/x/mod/semver"
 	"golang.org/x/pkgsite/internal"
 	"golang.org/x/pkgsite/internal/derrors"
-	"golang.org/x/pkgsite/internal/experiment"
 	"golang.org/x/pkgsite/internal/log"
 	"golang.org/x/pkgsite/internal/middleware"
 	"golang.org/x/pkgsite/internal/postgres"
@@ -158,159 +155,6 @@ func (s *Server) serveDetails(w http.ResponseWriter, r *http.Request, ds interna
 	return s.serveUnitPage(ctx, w, r, ds, um, urlInfo.requestedVersion)
 }
 
-type urlPathInfo struct {
-	// fullPath is the full import path corresponding to the requested
-	// package/module/directory page.
-	fullPath string
-	// isModule indicates whether the /mod page should be shown.
-	isModule bool
-	// modulePath is the path of the module corresponding to the fullPath and
-	// resolvedVersion. If unknown, it is set to internal.UnknownModulePath.
-	modulePath string
-	// requestedVersion is the version requested by the user, which will be one
-	// of the following: "latest", "master", a Go version tag, or a semantic
-	// version.
-	requestedVersion string
-	// resolvedVersion is the semantic version stored in the database.
-	resolvedVersion string
-}
-
-func extractURLPathInfo(urlPath string) (_ *urlPathInfo, err error) {
-	defer derrors.Wrap(&err, "extractURLPathInfo(%q)", urlPath)
-
-	info := &urlPathInfo{}
-	if strings.HasPrefix(urlPath, "/mod/") {
-		urlPath = strings.TrimPrefix(urlPath, "/mod")
-		info.isModule = true
-	}
-	// Parse the fullPath, modulePath and requestedVersion, based on whether
-	// the path is in the stdlib. If unable to parse these elements, return
-	// http.StatusBadRequest.
-	if parts := strings.SplitN(strings.TrimPrefix(urlPath, "/"), "@", 2); stdlib.Contains(parts[0]) {
-		info.fullPath, info.requestedVersion, err = parseStdLibURLPath(urlPath)
-		info.modulePath = stdlib.ModulePath
-		if info.fullPath == stdlib.ModulePath {
-			info.isModule = true
-		}
-	} else {
-		info.fullPath, info.modulePath, info.requestedVersion, err = parseDetailsURLPath(urlPath)
-	}
-	if err != nil {
-		return nil, err
-	}
-	return info, nil
-}
-
-// parseDetailsURLPath parses a URL path that refers (or may refer) to something
-// in the Go ecosystem.
-//
-// After trimming leading and trailing slashes, the path is expected to have one
-// of three forms, and we divide it into three parts: a full path, a module
-// path, and a version.
-//
-// 1. The path has no '@', like github.com/hashicorp/vault/api.
-//    This is the full path. The module path is unknown. So is the version, so we
-//    treat it as the latest version for whatever the path denotes.
-//
-// 2. The path has "@version" at the end, like github.com/hashicorp/vault/api@v1.2.3.
-//    We split this at the '@' into a full path (github.com/hashicorp/vault/api)
-//    and version (v1.2.3); the module path is still unknown.
-//
-// 3. The path has "@version" in the middle, like github.com/hashicorp/vault@v1.2.3/api.
-//    (We call this the "canonical" form of a path.)
-//    We remove the version to get the full path, which is again
-//    github.com/hashicorp/vault/api. The version is v1.2.3, and the module path is
-//    the part before the '@', github.com/hashicorp/vault.
-//
-// In one case, we do a little more than parse the urlPath into parts: if the full path
-// could be a part of the standard library (because it has no '.'), we assume it
-// is and set the modulePath to indicate the standard library.
-func parseDetailsURLPath(urlPath string) (fullPath, modulePath, requestedVersion string, err error) {
-	defer derrors.Wrap(&err, "parseDetailsURLPath(%q)", urlPath)
-
-	// This splits urlPath into either:
-	//   /<module-path>[/<suffix>]
-	// or
-	//   /<module-path>, @<version>/<suffix>
-	// or
-	//  /<module-path>/<suffix>, @<version>
-	parts := strings.SplitN(urlPath, "@", 2)
-	basePath := strings.TrimSuffix(strings.TrimPrefix(parts[0], "/"), "/")
-	if len(parts) == 1 { // no '@'
-		modulePath = internal.UnknownModulePath
-		requestedVersion = internal.LatestVersion
-		fullPath = basePath
-	} else {
-		// Parse the version and suffix from parts[1], the string after the '@'.
-		endParts := strings.Split(parts[1], "/")
-		suffix := strings.Join(endParts[1:], "/")
-		// The first path component after the '@' is the version.
-		requestedVersion = endParts[0]
-		// You cannot explicitly write "latest" for the version.
-		if requestedVersion == internal.LatestVersion {
-			return "", "", "", fmt.Errorf("invalid version: %q", requestedVersion)
-		}
-		if suffix == "" {
-			// "@version" occurred at the end of the path; we don't know the module path.
-			modulePath = internal.UnknownModulePath
-			fullPath = basePath
-		} else {
-			// "@version" occurred in the middle of the path; the part before it
-			// is the module path.
-			modulePath = basePath
-			fullPath = basePath + "/" + suffix
-		}
-	}
-	// The full path must be a valid import path (that is, package path), even if it denotes
-	// a module, directory or collection.
-	if err := module.CheckImportPath(fullPath); err != nil {
-		return "", "", "", fmt.Errorf("malformed path %q: %v", fullPath, err)
-	}
-
-	// If the full path is (or could be) in the standard library, change the
-	// module path to say so. But in that case, disallow versions in the middle,
-	// like "net@go1.14/http". That says that the module is "net", and it isn't.
-	if stdlib.Contains(fullPath) {
-		if modulePath != internal.UnknownModulePath {
-			return "", "", "", fmt.Errorf("non-final version in standard library path %q", urlPath)
-		}
-		modulePath = stdlib.ModulePath
-	}
-	return fullPath, modulePath, requestedVersion, nil
-}
-
-// validatePathAndVersion verifies that the requested path and version are
-// acceptable. The given path may be a module or package path.
-func validatePathAndVersion(ctx context.Context, ds internal.DataSource, fullPath, requestedVersion string) error {
-	if !isSupportedVersion(fullPath, requestedVersion) {
-		return invalidVersionError(fullPath, requestedVersion)
-	}
-	db, ok := ds.(*postgres.DB)
-	if !ok {
-		return nil
-	}
-	excluded, err := db.IsExcluded(ctx, fullPath)
-	if err != nil {
-		return err
-	}
-	if excluded {
-		// Return NotFound; don't let the user know that the package was excluded.
-		return &serverError{status: http.StatusNotFound}
-	}
-	return nil
-}
-
-// isSupportedVersion reports whether the version is supported by the frontend.
-func isSupportedVersion(fullPath, requestedVersion string) bool {
-	if stdlib.Contains(fullPath) && requestedVersion == internal.MasterVersion {
-		return false
-	}
-	if requestedVersion == internal.LatestVersion || semver.IsValid(requestedVersion) {
-		return true
-	}
-	return requestedVersion == internal.MasterVersion
-}
-
 // pathNotFoundError returns a page with an option on how to
 // add a package or module to the site.
 func pathNotFoundError(fullPath, requestedVersion string) error {
@@ -355,27 +199,6 @@ func proxydatasourceNotSupportedErr() error {
 				`<h3 class="Error-message">This page is not supported by the proxydatasource.</h3>`),
 		},
 	}
-}
-
-func parseStdLibURLPath(urlPath string) (path, requestedVersion string, err error) {
-	defer derrors.Wrap(&err, "parseStdLibURLPath(%q)", urlPath)
-
-	// This splits urlPath into either:
-	//   /<path>@<tag> or /<path>
-	parts := strings.SplitN(urlPath, "@", 2)
-	path = strings.TrimSuffix(strings.TrimPrefix(parts[0], "/"), "/")
-	if err := module.CheckImportPath(path); err != nil {
-		return "", "", err
-	}
-
-	if len(parts) == 1 {
-		return path, internal.LatestVersion, nil
-	}
-	requestedVersion = stdlib.VersionForTag(strings.TrimSuffix(parts[1], "/"))
-	if requestedVersion == "" {
-		return "", "", fmt.Errorf("invalid Go tag for url: %q", urlPath)
-	}
-	return path, requestedVersion, nil
 }
 
 // errUnitNotFoundWithoutFetch returns a 404 with instructions to the user on
@@ -450,44 +273,4 @@ func recordVersionTypeMetric(ctx context.Context, requestedVersion string) {
 	stats.RecordWithTags(ctx, []tag.Mutator{
 		tag.Upsert(keyVersionType, v),
 	}, versionTypeResults.M(1))
-}
-
-func setExperimentsFromQueryParam(ctx context.Context, r *http.Request) context.Context {
-	if err := r.ParseForm(); err != nil {
-		log.Errorf(ctx, "ParseForm: %v", err)
-		return ctx
-	}
-	return newContextFromExps(ctx, r.Form["exp"])
-}
-
-// newContextFromExps adds and removes experiments from the context's experiment
-// set, creates a new set with the changes, and returns a context with the new
-// set. Each string in expMods can be either an experiment name, which means
-// that the experiment should be added, or "!" followed by an experiment name,
-// meaning that it should be removed.
-func newContextFromExps(ctx context.Context, expMods []string) context.Context {
-	var (
-		exps   []string
-		remove = map[string]bool{}
-	)
-	set := experiment.FromContext(ctx)
-	for _, exp := range expMods {
-		if strings.HasPrefix(exp, "!") {
-			exp = exp[1:]
-			if set.IsActive(exp) {
-				remove[exp] = true
-			}
-		} else if !set.IsActive(exp) {
-			exps = append(exps, exp)
-		}
-	}
-	if len(exps) == 0 && len(remove) == 0 {
-		return ctx
-	}
-	for _, a := range set.Active() {
-		if !remove[a] {
-			exps = append(exps, a)
-		}
-	}
-	return experiment.NewContext(ctx, exps...)
 }
