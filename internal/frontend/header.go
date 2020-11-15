ed116dea3a90937c6ae9471c8b25867aa14d6eb5
Julie Qiu <julie@golang.org>
2020-11-15 15:59:02 -0500

    internal/frontend: delete Package and Module
    The Package and Module structs in internal/frontend are no longer used
    and are deleted.
    
    Change-Id: I196a2491ec6e4b07a6dec13030e1985bbcbc8300
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/270249
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>

--- internal/frontend/header.go
+++ internal/frontend/header.go
@@ -13,30 +13,6 @@ import (
 	"golang.org/x/pkgsite/internal/stdlib"
 )
 
-// Package contains information for an individual package.
-type Package struct {
-	Module
-	Path               string // full import path
-	URL                string // relative to this site
-	LatestURL          string // link with latest-version placeholder, relative to this site
-	IsRedistributable  bool
-	Licenses           []LicenseMetadata
-	PathAfterDirectory string // for display on the directories tab; used by Directory
-	Synopsis           string // for display on the directories tab; used by Directory
-}
-
-// Module contains information for an individual module.
-type Module struct {
-	DisplayVersion    string
-	LinkVersion       string
-	ModulePath        string
-	CommitTime        string
-	IsRedistributable bool
-	URL               string // relative to this site
-	LatestURL         string // link with latest-version placeholder, relative to this site
-	Licenses          []LicenseMetadata
-}
-
 func constructPackageURL(pkgPath, modulePath, linkVersion string) string {
 	if linkVersion == internal.LatestVersion {
 		return "/" + pkgPath
