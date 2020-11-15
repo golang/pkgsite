ce3790d2ce70cad23df58275d643126af901e020
Julie Qiu <julie@golang.org>
2020-11-15 16:00:04 -0500

    internal/frontend: merge unit_header.go into header.go
    Code in unit_header.go is merged into header.go. Pure code in motion.
    
    Change-Id: I551e667980bd5b3d289b88c7be762cd985a998ec
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/270250
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Trust: Julie Qiu <julie@golang.org>

--- internal/frontend/header.go
+++ internal/frontend/header.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Go Authors. All rights reserved.
+// Copyright 2020 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,12 +6,96 @@ package frontend
 
 import (
 	"fmt"
+	"path"
 	"strings"
 	"time"
 
+	"golang.org/x/mod/module"
 	"golang.org/x/pkgsite/internal"
 	"golang.org/x/pkgsite/internal/stdlib"
 )
+
+const (
+	pageTypeModule    = "module"
+	pageTypeDirectory = "directory"
+	pageTypePackage   = "package"
+	pageTypeCommand   = "command"
+	pageTypeModuleStd = "std"
+	pageTypeStdlib    = "standard library"
+)
+
+// pageTitle determines the pageTitles for a given unit.
+// See TestPageTitlesAndTypes for examples.
+func pageTitle(um *internal.UnitMeta) string {
+	switch {
+	case um.Path == stdlib.ModulePath:
+		return "Standard library"
+	case um.IsCommand():
+		return effectiveName(um.Path, um.Name)
+	case um.IsPackage():
+		return um.Name
+	case um.IsModule():
+		return path.Base(um.Path)
+	default:
+		return path.Base(um.Path) + "/"
+	}
+}
+
+// pageType determines the pageType for a given unit.
+func pageType(um *internal.UnitMeta) string {
+	if um.Path == stdlib.ModulePath {
+		return pageTypeModuleStd
+	}
+	if um.IsCommand() {
+		return pageTypeCommand
+	}
+	if um.IsPackage() {
+		return pageTypePackage
+	}
+	if um.IsModule() {
+		return pageTypeModule
+	}
+	return pageTypeDirectory
+}
+
+// pageLabels determines the labels to display for a given unit.
+// See TestPageTitlesAndTypes for examples.
+func pageLabels(um *internal.UnitMeta) []string {
+	var pageTypes []string
+	if um.Path == stdlib.ModulePath {
+		return nil
+	}
+	if um.IsCommand() {
+		pageTypes = append(pageTypes, pageTypeCommand)
+	} else if um.IsPackage() {
+		pageTypes = append(pageTypes, pageTypePackage)
+	}
+	if um.IsModule() {
+		pageTypes = append(pageTypes, pageTypeModule)
+	}
+	if !um.IsPackage() && !um.IsModule() {
+		pageTypes = append(pageTypes, pageTypeDirectory)
+	}
+	if stdlib.Contains(um.Path) {
+		pageTypes = append(pageTypes, pageTypeStdlib)
+	}
+	return pageTypes
+}
+
+// effectiveName returns either the command name or package name.
+func effectiveName(pkgPath, pkgName string) string {
+	if pkgName != "main" {
+		return pkgName
+	}
+	var prefix string // package path without version
+	if pkgPath[len(pkgPath)-3:] == "/v1" {
+		prefix = pkgPath[:len(pkgPath)-3]
+	} else {
+		prefix, _, _ = module.SplitPathVersion(pkgPath)
+	}
+	_, base := path.Split(prefix)
+	return base
+}
 
 func constructPackageURL(pkgPath, modulePath, linkVersion string) string {
 	if linkVersion == internal.LatestVersion {
--- internal/frontend/header_test.go
+++ internal/frontend/header_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Go Authors. All rights reserved.
+// Copyright 2020 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -8,8 +8,80 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/pkgsite/internal"
+	"golang.org/x/pkgsite/internal/stdlib"
 	"golang.org/x/pkgsite/internal/testing/sample"
 )
+
+type testUnitPage struct {
+	unit       *internal.UnitMeta
+	name       string
+	wantTitle  string
+	wantType   string
+	wantLabels []string
+}
+
+func TestPageTitlePageTypePageLabels(t *testing.T) {
+
+	var tests []*testUnitPage
+	m := sample.Module("golang.org/x/tools", "v1.0.0", "go/packages", "cmd/godoc")
+	for _, u := range m.Units {
+		um := &u.UnitMeta
+		switch um.Path {
+		case "golang.org/x/tools":
+			tests = append(tests, &testUnitPage{um, "module golang.org/x/tools", "tools", pageTypeModule, []string{pageTypeModule}})
+		case "golang.org/x/tools/go/packages":
+			tests = append(tests, &testUnitPage{um, "package golang.org/x/tools/go/packages", "packages", pageTypePackage, []string{pageTypePackage}})
+		case "golang.org/x/tools/go":
+			tests = append(tests, &testUnitPage{um, "directory golang.org/x/tools/go", "go/", pageTypeDirectory, []string{pageTypeDirectory}})
+		case "golang.org/x/tools/cmd/godoc":
+			um.Name = "main"
+			tests = append(tests, &testUnitPage{um, "package golang.org/x/tools/cmd/godoc", "godoc", pageTypeCommand, []string{pageTypeCommand}})
+		case "golang.org/x/tools/cmd":
+			tests = append(tests, &testUnitPage{um, "directory golang.org/x/tools/cmd", "cmd/", pageTypeDirectory, []string{pageTypeDirectory}})
+		default:
+			t.Fatalf("Unexpected path: %q", um.Path)
+		}
+	}
+
+	m2 := sample.Module("golang.org/x/tools/gopls", "v1.0.0", "")
+	m2.Units[0].Name = "main"
+	tests = append(tests, &testUnitPage{&m2.Units[0].UnitMeta, "module golang.org/x/tools/gopls", "gopls", pageTypeCommand, []string{pageTypeCommand, pageTypeModule}})
+
+	std := sample.Module(stdlib.ModulePath, "v1.0.0", "cmd/go")
+	for _, u := range std.Units {
+		um := &u.UnitMeta
+		switch um.Path {
+		case stdlib.ModulePath:
+			tests = append(tests, &testUnitPage{um, "module std", "Standard library", pageTypeModuleStd, nil})
+		case "cmd":
+			tests = append(tests, &testUnitPage{um, "directory cmd", "cmd/", pageTypeDirectory, []string{pageTypeDirectory, pageTypeStdlib}})
+		case "cmd/go":
+			um.Name = "main"
+			tests = append(tests, &testUnitPage{um, "command go", "go", pageTypeCommand, []string{pageTypeCommand, pageTypeStdlib}})
+		default:
+			t.Fatalf("Unexpected path: %q", um.Path)
+		}
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotTitle := pageTitle(test.unit)
+			if gotTitle != test.wantTitle {
+				t.Errorf("pageTitle(%q): %q; want = %q", test.unit.Path, gotTitle, test.wantTitle)
+			}
+			gotType := pageType(test.unit)
+			if gotType != test.wantType {
+				t.Errorf("pageType(%q): %q; want = %q", test.unit.Path, gotType, test.wantType)
+			}
+			gotLabels := pageLabels(test.unit)
+			if diff := cmp.Diff(test.wantLabels, gotLabels); diff != "" {
+				t.Errorf("mismatch on pageLabels (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
 
 func TestAbsoluteTime(t *testing.T) {
 	now := sample.NowTruncated()
