c38d0c433f625701bcfa3c6b9238920bc0f53061
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-11-28 15:35:27 +0000

    devtools: make create_local_db.sh respect env vars
    The Postgres contribution guide (/doc/postgres.md) tells the user to
    set specific environment variables and then run the create_local_db
    script. However, the create_local_db script did not respect those
    variables. This was causing issues for some local configurations.
    
    Changes the script to respect the environment variables if set,
    otherwise fall back on the postgres defaults detailed in the guide.
    
    Change-Id: Ib84622266ae1a841f7a17a84ee020ac8b6ba30a6
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/273846
    Run-TryBot: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>
    Trust: Jonathan Amsterdam <jba@google.com>

--- devtools/create_local_db.sh
+++ devtools/create_local_db.sh
@@ -8,8 +8,29 @@
 
 source devtools/lib.sh || { echo "Are you at repo root?"; exit 1; }
 
-echo "CREATE DATABASE \"discovery-db\" \
-        OWNER = postgres \
+database_user="postgres"
+if [[ $GO_DISCOVERY_DATABASE_USER != "" ]]; then
+  database_user=$GO_DISCOVERY_DATABASE_USER
+fi
+
+database_password=""
+if [[ $GO_DISCOVERY_DATABASE_PASSWORD != "" ]]; then
+  database_password=$GO_DISCOVERY_DATABASE_PASSWORD
+fi
+
+database_host="localhost"
+if [[ $GO_DISCOVERY_DATABASE_HOST != "" ]]; then
+  database_host=$GO_DISCOVERY_DATABASE_HOST
+fi
+
+database_name='discovery-db'
+if [[ $GO_DISCOVERY_DATABASE_NAME != "" ]]; then
+  database_name=$GO_DISCOVERY_DATABASE_NAME
+fi
+
+echo "CREATE DATABASE \"$database_name\" \
+        OWNER = $database_user \
         TEMPLATE=template0 \
         LC_COLLATE = 'C' \
-        LC_CTYPE = 'C';" | psql 'host=127.0.0.1 sslmode=disable user=postgres'
+        LC_CTYPE = 'C';" | psql postgresql://$database_user:$database_password@$database_host:5432?sslmode=disable
+
