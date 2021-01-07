281be9fa2a92a2903940fed6e9d59ff24c4ce4ed
Julie Qiu <julie@golang.org>
2021-01-07 09:30:56 -0500

    migrations: add unique(path_id, module_id) on units
    The NOT NULL constraint on units.path is dropped.
    
    A NOT NULL constraint is set on units.path_id.
    
    A UNIQUE constraint is added to (path_id, module_id).
    
    Change-Id: I46cd7fbe002378696e0ed3243241dbced9a4c8f5
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/282472
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000054_add_units_id_module_id_unique.down.sql
@@ -0,0 +1,11 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE units DROP CONSTRAINT units_path_id_module_id_key;
+ALTER TABLE units ALTER COLUMN path_id DROP NOT NULL;
+ALTER TABLE units ALTER COLUMN path ADD NOT NULL;
+
+END;
