c977a4fbcb632a53b1e2a81f91927fcbe51e1da6
Julie Qiu <julie@golang.org>
2021-01-14 17:32:25 -0500

    migrations: set units.v1path_id NOT NULL
    units.v1path_id has been populated and should now be non-nullable.
    
    Change-Id: I20e363e7b6b7bb59bb52b73f465b896e96103750
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/283954
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Trust: Julie Qiu <julie@golang.org>

---
+++ migrations/000058_units_v1path_id_not_null.up.sql
@@ -0,0 +1,9 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE units ALTER COLUMN v1path_id SET NOT NULL;
+
+END;
