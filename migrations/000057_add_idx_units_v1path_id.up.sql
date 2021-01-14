1ba423dfad7b83a5be20d0e85081c3aa80be25ea
Julie Qiu <julie@golang.org>
2021-01-14 08:35:52 -0500

    migrations: add index on units.v1path_id
    Change-Id: Ic4611f62209bf324c9987b9626a6ed200d93bd19
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/283645
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000057_add_idx_units_v1path_id.up.sql
@@ -0,0 +1,5 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+CREATE INDEX CONCURRENTLY idx_units_v1path_id ON units(v1path_id);
