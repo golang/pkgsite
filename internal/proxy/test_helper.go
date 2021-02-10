ff5a7a3f635d9c56780c2a176964dca828dfe9cd
Jonathan Amsterdam <jba@google.com>
2021-02-10 16:14:14 -0500

    internal/proxy/testdata: add standard examples
    Add some examples that will be used in tests,
    and provide a way to load them into proxy.Modules.
    
    For golang/go#44214
    
    Change-Id: Ia05b8d8e27071d821532e8747e1ab3b6e05085a0
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/290894
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>

--- internal/proxy/test_helper.go
+++ internal/proxy/test_helper.go
@@ -5,14 +5,17 @@
 package proxy
 
 import (
+	"fmt"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"golang.org/x/pkgsite/internal/testing/testhelper"
+	"golang.org/x/tools/txtar"
 )
 
 // SetupTestClient creates a fake module proxy for testing using the given test
-// version information. If modules is nil, it will default to hosting the
-// modules in the testdata directory.
+// version information.
 //
 // It returns a function for tearing down the proxy after the test is completed
 // and a Client for interacting with the test proxy.
@@ -37,4 +40,70 @@ func NewClientForServer(s *Server) (*Client, func(), error) {
 	}
 	client.httpClient = httpClient
 	return client, serverClose, nil
+}
+
+// LoadTestModules reads the modules in the given directory. Each file in that
+// directory with a .txtar extension should be named "path@version" and should
+// be in txtar format (golang.org/x/tools/txtar). The path part of the filename
+// will be preceded by "example.com/" and colons will be replaced by slashes to
+// form a full module path. The file contents are used verbatim except that some
+// variables beginning with "$" are substituted with predefined strings.
+//
+// LoadTestModules panics if there is an error reading any of the files.
+func LoadTestModules(dir string) []*Module {
+	files, err := filepath.Glob(filepath.Join(dir, "*.txtar"))
+	if err != nil {
+		panic(err)
+	}
+	var ms []*Module
+	for _, f := range files {
+		m, err := readTxtarModule(f)
+		if err != nil {
+			panic(err)
+		}
+		ms = append(ms, m)
+	}
+	return ms
+}
+
+var testModuleReplacer = strings.NewReplacer(
+	"$MITLicense", testhelper.MITLicense,
+	"$BSD0License", testhelper.BSD0License,
+)
+
+func readTxtarModule(filename string) (*Module, error) {
+	modver := strings.TrimSuffix(filepath.Base(filename), filepath.Ext(filename))
+	i := strings.IndexRune(modver, '@')
+	if i < 0 {
+		return nil, fmt.Errorf("%s: filename missing '@'", modver)
+	}
+	modulePath, version := "example.com/"+modver[:i], modver[i+1:]
+	modulePath = strings.ReplaceAll(modulePath, ":", "/")
+	if modulePath == "" || version == "" {
+		return nil, fmt.Errorf("%s: empty module path or version", filename)
+	}
+	m := &Module{
+		ModulePath: modulePath,
+		Version:    version,
+		Files:      map[string]string{},
+	}
+	ar, err := txtar.ParseFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range ar.Files {
+		m.Files[f.Name] = strings.TrimSpace(testModuleReplacer.Replace(string(f.Data)))
+	}
+	return m, nil
+}
+
+// FindModule returns the module in mods with the given path and version, or nil
+// if there isn't one. An empty version argument matches any version.
+func FindModule(mods []*Module, path, version string) *Module {
+	for _, m := range mods {
+		if m.ModulePath == path && (version == "" || m.Version == version) {
+			return m
+		}
+	}
+	return nil
 }
