23a500435c2a27f8b63ae7375b98ecc5f64f99cc
Jonathan Amsterdam <jba@google.com>
2021-02-12 10:20:27 -0500

    internal/derrors: add stack trace
    Add StackError, an error with a stack trace.
    
    Add the WrapStack function to add one while wrapping.
    
    We can use these to capture a stack lower down for
    later reporting, when we have more context.
    
    For golang/go#44231
    
    Change-Id: Ib00aa75daa46e60ced99bfa5f1fd65bb31828d01
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/291490
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

--- internal/derrors/derrors.go
+++ internal/derrors/derrors.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"runtime"
 
 	"cloud.google.com/go/errorreporting"
 )
@@ -224,6 +225,42 @@ func Wrap(errp *error, format string, args ...interface{}) {
 	if *errp != nil {
 		*errp = fmt.Errorf("%s: %w", fmt.Sprintf(format, args...), *errp)
 	}
+}
+
+// WrapStack is like Wrap, but adds a stack trace if there isn't one already.
+func WrapStack(errp *error, format string, args ...interface{}) {
+	if *errp != nil {
+		if se := (*StackError)(nil); !errors.As(*errp, &se) {
+			*errp = NewStackError(*errp)
+		}
+		Wrap(errp, format, args...)
+	}
+}
+
+// StackError wraps an error and adds a stack trace.
+type StackError struct {
+	Stack []byte
+	err   error
+}
+
+// NewStackError returns a StackError, capturing a stack trace.
+func NewStackError(err error) *StackError {
+	// Limit the stack trace to 16K. Same value used in the errorreporting client,
+	// cloud.google.com/go@v0.66.0/errorreporting/errors.go.
+	var buf [16 * 1024]byte
+	n := runtime.Stack(buf[:], false)
+	return &StackError{
+		err:   err,
+		Stack: buf[:n],
+	}
+}
+
+func (e *StackError) Error() string {
+	return e.err.Error() // ignore the stack
+}
+
+func (e *StackError) Unwrap() error {
+	return e.err
 }
 
 // WrapAndReport calls Wrap followed by Report.
--- internal/derrors/derrors_test.go
+++ internal/derrors/derrors_test.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -97,5 +98,20 @@ func TestWrap(t *testing.T) {
 	}
 	if got := errors.Unwrap(err); got != orig {
 		t.Errorf("Unwrap: got %#v, want %#v", got, orig)
+	}
+}
+
+func TestWrapStack(t *testing.T) {
+	var err error = io.ErrShortWrite
+	WrapStack(&err, "while frobbing")
+	if !errors.Is(err, io.ErrShortWrite) {
+		t.Error("is not io.ErrShortWrite")
+	}
+	var se *StackError
+	if !errors.As(err, &se) {
+		t.Fatal("not as StackError")
+	}
+	if !strings.Contains(string(se.Stack), "WrapStack") {
+		t.Fatal("bad stack trace")
 	}
 }
