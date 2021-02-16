34eb050288f1b4176e3bf15dfafc1c7aa00ab14b
Jamal Carvalho <jamal@golang.org>
2021-02-16 14:49:46 -0500

    e2e: add and update snapshot tests
    Updates snapshot test for the homepage and
    adds a new test for golang/x/pkgsite.
    
    Change-Id: Iefe75f2e4e07eea797970020a406636ae2161fdb
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/292700
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- e2e/pkgsite.test.ts
+++ e2e/pkgsite.test.ts
@@ -7,7 +7,7 @@
 
 import puppeteer, { Page } from 'puppeteer';
 
-describe('Homepage', () => {
+describe('golang.org/x/pkgsite', () => {
   const baseUrl = process.env.FRONTEND_URL ?? '';
   let page: Page;
   let browser: puppeteer.Browser;
@@ -17,11 +17,17 @@ describe('Homepage', () => {
       args: ['--no-sandbox', '--disable-dev-shm-usage'],
       defaultViewport: { height: 800, width: 1280 },
     });
-  });
+
+    page = await browser.newPage();
+    await page.goto(baseUrl);
+    await page.evaluate(() =>
+      fetch(`/fetch/golang.org/x/pkgsite@v0.0.0-20210216165259-5867665b19ca`, { method: 'POST' })
+    );
+  }, 30000);
 
   beforeEach(async () => {
     page = await browser.newPage();
-    await page.goto(baseUrl);
+    await page.goto(baseUrl + '/golang.org/x/pkgsite');
   });
 
   afterEach(async () => await page.close());
