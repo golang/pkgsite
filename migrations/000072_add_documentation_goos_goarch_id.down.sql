c50a924c56ce0aa95422b22ef849c0c893679415
Julie Qiu <julie@golang.org>
2021-02-24 12:59:35 -0500

    migration: add documentation.id, new_goos, and new_goarch
    new_goos and new_goarch are the same as goos and goarch, but they use
    the goos and goarch enum instead of a string.
    
    A trigger is added to populate documentation.id on insert or update.
    
    For golang/go#37102
    
    Change-Id: I2c69b476e2d52474a865035fc90e42e8587266a4
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/295952
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000072_add_documentation_goos_goarch_id.down.sql
@@ -0,0 +1,14 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE documentation DROP COLUMN id;
+ALTER TABLE documentation DROP COLUMN new_goos;
+ALTER TABLE documentation DROP COLUMN new_goarch;
+DROP TRIGGER documentation_id_update ON documentation;
+DROP FUNCTION update_documentation_id;
+DROP SEQUENCE sequence_documentation_id;
+
+END;
---
+++ migrations/000072_add_documentation_goos_goarch_id.up.sql
@@ -0,0 +1,23 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE documentation ADD COLUMN id INTEGER;
+ALTER TABLE documentation ADD COLUMN new_goos goos;
+ALTER TABLE documentation ADD COLUMN new_goarch goarch;
+
+CREATE SEQUENCE sequence_documentation_id START 1;
+CREATE OR REPLACE FUNCTION update_documentation_id() RETURNS TRIGGER AS $BODY$
+BEGIN
+    NEW.id=nextval('sequence_documentation_id');
+    RETURN NEW;
+END
+$BODY$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER documentation_id_update
+BEFORE INSERT OR UPDATE ON documentation
+FOR EACH ROW EXECUTE PROCEDURE update_documentation_id();
+
+END;
