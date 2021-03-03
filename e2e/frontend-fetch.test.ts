7b81365ee8c925d80bbdc4e640bef4e7b7fc8bd7
Jamal Carvalho <jamal@golang.org>
2021-03-03 17:01:41 -0500

    e2e: add test for frontend fetch
    Change-Id: Ie2a8d7a46e00f9f25d6c319899bab26987924bb9
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/298430
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Julie Qiu <julie@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ e2e/frontend-fetch.test.ts
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright 2020 The Go Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+import './globals';
+import puppeteer from 'puppeteer';
+
+const baseUrl = process.env.FRONTEND_URL ?? '';
+
+const selectors = {
+  fetchButton: '.js-fetchButton',
+  fetchMessage: '.js-fetchMessage',
+};
+
+describe('Frontend Fetch', () => {
+  beforeAll(async () => {
+    await page.goto(baseUrl + '/golang.org/x/tools/gopls@v0.6.6');
+  });
+
+  test('accessibility tree matches snapshot', async () => {
+    const a11yTree = await page.accessibility.snapshot();
+    expect(a11yTree).toMatchSnapshot();
+  });
+
+  test('desktop viewport matches image snapshot', async () => {
+    const image = await page.screenshot({ fullPage: true });
+    expect(image).toMatchImageSnapshot();
+  });
+
+  test('mobile viewport matches image snapshot', async () => {
+    await page.emulate(puppeteer.devices['Pixel 2']);
+    const image = await page.screenshot({ fullPage: true });
+    expect(image).toMatchImageSnapshot();
+  });
+
+  test('clicking fetch button fetches module and navigates to page', async () => {
+    expect(await page.title()).toBe('404 Not Found · pkg.go.dev');
+
+    await page.click(selectors.fetchButton);
+    const message = await page.$eval(selectors.fetchMessage, el => el.textContent);
+    expect(message).toBe('Fetching golang.org/x/tools/gopls@v0.6.6');
+
+    await page.waitForNavigation();
+    expect(await page.title()).toBe('gopls · pkg.go.dev');
+  }, 30000);
+});
