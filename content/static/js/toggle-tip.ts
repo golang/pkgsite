9826220f9b38189798f7d7c54712ce22f3854a0b
Jamal Carvalho <jamal@golang.org>
2021-03-05 16:08:52 -0500

    content/static: rewrite toggle-tip.js with typescript
    For golang/go#43359
    
    Change-Id: Ifca0779ae6a949c06c765b20d7a3e0f1c591f60a
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/299258
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>

--- content/static/js/toggle-tip.ts
+++ content/static/js/toggle-tip.ts
@@ -1,11 +1,17 @@
-'use strict';
 /*!
  * @license
  * Copyright 2019-2020 The Go Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
+
 const toggles = document.querySelectorAll('[data-toggletip-content]');
+
+/**
+ * Adds event listeners to toggletip elements to display their
+ * content when the toggle button is pressed. Used in the right
+ * sidebar details section.
+ */
 toggles.forEach(toggle => {
   const message = toggle.getAttribute('data-toggletip-content');
   const tip = toggle.nextElementSibling;
@@ -18,6 +24,8 @@ toggles.forEach(toggle => {
       tip.innerHTML = '<span class="UnitMetaDetails-toggletipBubble">' + message + '</span>';
     }, 100);
   });
+
+  // Close on outside click
   document.addEventListener('click', e => {
     if (toggle !== e.target) {
       if (!tip) {
@@ -26,13 +34,14 @@ toggles.forEach(toggle => {
       tip.innerHTML = '';
     }
   });
+
+  // Remove toggletip on ESC
   toggle.addEventListener('keydown', e => {
     if (!tip) {
       return;
     }
-    if (e.key === 'Escape') {
+    if ((e as KeyboardEvent).key === 'Escape') {
       tip.innerHTML = '';
     }
   });
 });
-//# sourceMappingURL=toggle-tip.js.map
