c9a527b474cc14763490b36f601df3bb9f70abce
Jamal Carvalho <jamal@golang.org>
2021-03-09 12:03:50 -0500

    content/static: rewrite sidenav.js with typescript
    For golang/go#43359
    
    Change-Id: Icd5d9bdf06694e823b898d7972ba2fb697454896
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/300069
    Trust: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Julie Qiu <julie@golang.org>

--- content/static/js/sidenav.ts
+++ content/static/js/sidenav.ts
@@ -1,9 +1,13 @@
-'use strict';
 /*!
  * @license
  * Copyright 2019-2021 The Go Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
+ */
+
+/**
+ * Possible KeyboardEvent key values.
+ * @private @enum {string}
  */
 const Key = {
   UP: 'ArrowUp',
@@ -15,17 +19,46 @@ const Key = {
   SPACE: ' ',
   END: 'End',
   HOME: 'Home',
+
+  // Global keyboard shortcuts.
+  // TODO(golang.org/issue/40246): consolidate keyboard shortcut handling to avoid
+  // this duplication.
   Y: 'y',
   FORWARD_SLASH: '/',
   QUESTION_MARK: '?',
 };
+
+/**
+ * The navigation tree component of the documentation page.
+ */
 class DocNavTreeController {
-  constructor(el) {
-    this.el = el;
-    this.focusedIndex = 0;
-    this.visibleItems = [];
-    this.searchString = '';
-    this.lastKeyDownTimeStamp = -Infinity;
+  /**
+   * The currently selected element.
+   */
+  private selectedEl: HTMLElement | null;
+  /**
+   * The index of the currently focused item. Used when navigating the tree
+   * using the keyboard.
+   */
+  private focusedIndex = 0;
+  /**
+   * The elements currently visible (not within a collapsed node of the tree).
+   */
+  private visibleItems: HTMLElement[] = [];
+  /**
+   * The current search string.
+   */
+  private searchString = '';
+  /**
+   * The timestamp of the last keydown event. Used to track whether to use the
+   * current search string.
+   */
+  private lastKeyDownTimeStamp = -Infinity;
+
+  /**
+   * Instantiates a navigation tree.
+   */
+  constructor(private el: Element) {
     this.el = el;
     this.selectedEl = null;
     this.focusedIndex = 0;
@@ -36,33 +69,49 @@ class DocNavTreeController {
     this.updateVisibleItems();
     this.initialize();
   }
-  initialize() {
+
+  /**
+   * Initializes the tree. Should be called only once.
+   */
+  private initialize() {
     this.el.querySelectorAll(`[role='treeitem']`).forEach(el => {
-      el.addEventListener('click', e => this.handleItemClick(e));
+      el.addEventListener('click', e => this.handleItemClick(e as MouseEvent));
     });
+
+    // TODO: remove once safehtml supports aria-owns with dynamic values.
     this.el.querySelectorAll('[data-aria-owns]').forEach(el => {
-      var _a;
-      el.setAttribute(
-        'aria-owns',
-        (_a = el.getAttribute('data-aria-owns')) !== null && _a !== void 0 ? _a : ''
-      );
+      el.setAttribute('aria-owns', el.getAttribute('data-aria-owns') ?? '');
     });
   }
-  addEventListeners() {
-    this.el.addEventListener('keydown', e => this.handleKeyDown(e));
+
+  private addEventListeners() {
+    this.el.addEventListener('keydown', e => this.handleKeyDown(e as KeyboardEvent));
   }
-  setFocusedIndex(index) {
+
+  /**
+   * Sets the visible item with the given index with the proper tabindex and
+   * focuses it.
+   */
+  setFocusedIndex(index: number) {
     if (index === this.focusedIndex || index === -1) {
       return;
     }
+
     let itemEl = this.visibleItems[this.focusedIndex];
     itemEl.setAttribute('tabindex', '-1');
+
     itemEl = this.visibleItems[index];
     itemEl.setAttribute('tabindex', '0');
     itemEl.focus();
+
     this.focusedIndex = index;
   }
-  setSelectedId(opt_id) {
+
+  /**
+   * Marks the navigation node with the given ID as selected. If no ID is
+   * provided, the first visible item in the tree is used.
+   */
+  setSelectedId(opt_id: string) {
     if (this.selectedEl) {
       this.selectedEl.removeAttribute('aria-selected');
       this.selectedEl = null;
@@ -72,13 +121,19 @@ class DocNavTreeController {
     } else if (this.visibleItems.length > 0) {
       this.selectedEl = this.visibleItems[0];
     }
+
     if (!this.selectedEl) {
       return;
     }
-    const topLevelExpanded = this.el.querySelector('[aria-level="1"][aria-expanded="true"]');
+
+    // Close inactive top level item if selected id is not in its tree.
+    const topLevelExpanded = this.el.querySelector<HTMLElement>(
+      '[aria-level="1"][aria-expanded="true"]'
+    );
     if (topLevelExpanded && !topLevelExpanded.contains(this.selectedEl)) {
       this.collapseItem(topLevelExpanded);
     }
+
     if (this.selectedEl.getAttribute('aria-level') === '1') {
       this.selectedEl.setAttribute('aria-expanded', 'true');
     }
@@ -86,7 +141,11 @@ class DocNavTreeController {
     this.expandAllParents(this.selectedEl);
     this.scrollElementIntoView(this.selectedEl);
   }
-  expandAllSiblingItems(el) {
+
+  /**
+   * Expands all sibling items of the given element.
+   */
+  private expandAllSiblingItems(el: HTMLElement) {
     const level = el.getAttribute('aria-level');
     this.el.querySelectorAll(`[aria-level='${level}'][aria-expanded='false']`).forEach(el => {
       el.setAttribute('aria-expanded', 'true');
@@ -94,7 +153,11 @@ class DocNavTreeController {
     this.updateVisibleItems();
     this.focusedIndex = this.visibleItems.indexOf(el);
   }
-  expandAllParents(el) {
+
+  /**
+   * Expands all parent items of the given element.
+   */
+  private expandAllParents(el: HTMLElement) {
     if (!this.visibleItems.includes(el)) {
       let owningItemEl = this.owningItem(el);
       while (owningItemEl) {
@@ -103,29 +166,44 @@ class DocNavTreeController {
       }
     }
   }
-  scrollElementIntoView(el) {
+
+  /**
+   * Scrolls the given element into view, aligning the element in the center
+   * of the viewport. If the element is already in view, no scrolling occurs.
+   */
+  private scrollElementIntoView(el: HTMLElement) {
     const STICKY_HEADER_HEIGHT_PX = 55;
     const viewportHeightPx = document.documentElement.clientHeight;
     const elRect = el.getBoundingClientRect();
     const verticalCenterPointPx = (viewportHeightPx - STICKY_HEADER_HEIGHT_PX) / 2;
     if (elRect.top < STICKY_HEADER_HEIGHT_PX) {
+      // Element is occluded at top of view by header or by being offscreen.
       this.el.scrollTop -=
         STICKY_HEADER_HEIGHT_PX - elRect.top - elRect.height + verticalCenterPointPx;
     } else if (elRect.bottom > viewportHeightPx) {
+      // Element is below viewport.
       this.el.scrollTop = elRect.bottom - viewportHeightPx + verticalCenterPointPx;
     } else {
       return;
     }
   }
-  handleItemClick(e) {
-    const el = e.target;
+
+  /**
+   * Handles when a tree item is clicked.
+   */
+  private handleItemClick(e: MouseEvent) {
+    const el = e.target as HTMLSelectElement;
     this.setFocusedIndex(this.visibleItems.indexOf(el));
     if (el.hasAttribute('aria-expanded')) {
       this.toggleItemExpandedState(el);
     }
     this.closeInactiveDocNavGroups(el);
   }
-  closeInactiveDocNavGroups(el) {
+
+  /**
+   * Closes inactive top level nav groups when a new tree item clicked.
+   */
+  private closeInactiveDocNavGroups(el: HTMLElement) {
     if (el.hasAttribute('aria-expanded')) {
       const level = el.getAttribute('aria-level');
       document.querySelectorAll(`[aria-level="${level}"]`).forEach(nav => {
@@ -137,8 +215,13 @@ class DocNavTreeController {
       this.focusedIndex = this.visibleItems.indexOf(el);
     }
   }
-  handleKeyDown(e) {
-    const targetEl = e.target;
+
+  /**
+   * Handles when a key is pressed when the component is in focus.
+   */
+  handleKeyDown(e: KeyboardEvent) {
+    const targetEl = e.target as HTMLElement | null;
+
     switch (e.key) {
       case Key.ASTERISK:
         if (targetEl) {
@@ -147,61 +230,77 @@ class DocNavTreeController {
         e.stopPropagation();
         e.preventDefault();
         return;
+
+      // Global keyboard shortcuts.
+      // TODO(golang.org/issue/40246): consolidate keyboard shortcut handling
+      // to avoid this duplication.
       case Key.FORWARD_SLASH:
       case Key.QUESTION_MARK:
         return;
+
       case Key.DOWN:
         this.focusNextItem();
         break;
+
       case Key.UP:
         this.focusPreviousItem();
         break;
+
       case Key.LEFT:
-        if (
-          (targetEl === null || targetEl === void 0
-            ? void 0
-            : targetEl.getAttribute('aria-expanded')) === 'true'
-        ) {
+        if (targetEl?.getAttribute('aria-expanded') === 'true') {
           this.collapseItem(targetEl);
         } else {
           this.focusParentItem(targetEl);
         }
         break;
+
       case Key.RIGHT: {
-        switch (
-          targetEl === null || targetEl === void 0 ? void 0 : targetEl.getAttribute('aria-expanded')
-        ) {
+        switch (targetEl?.getAttribute('aria-expanded')) {
           case 'false':
             this.expandItem(targetEl);
             break;
           case 'true':
+            // Select the first child.
             this.focusNextItem();
             break;
         }
         break;
       }
+
       case Key.HOME:
         this.setFocusedIndex(0);
         break;
+
       case Key.END:
         this.setFocusedIndex(this.visibleItems.length - 1);
         break;
+
       case Key.ENTER:
-        if ((targetEl === null || targetEl === void 0 ? void 0 : targetEl.tagName) === 'A') {
+        if (targetEl?.tagName === 'A') {
+          // Enter triggers desired behavior by itself.
           return;
         }
+      // Fall through for non-anchor items to be handled the same as when
+      // the space key is pressed.
+      // eslint-disable-next-line no-fallthrough
       case Key.SPACE:
-        targetEl === null || targetEl === void 0 ? void 0 : targetEl.click();
+        targetEl?.click();
         break;
+
       default:
+        // Could be a typeahead search.
         this.handleSearch(e);
         return;
     }
     e.preventDefault();
     e.stopPropagation();
   }
-  handleSearch(e) {
-    var _a;
+
+  /**
+   * Handles when a key event isn’t matched by shortcut handling, indicating
+   * that the user may be attempting a typeahead search.
+   */
+  private handleSearch(e: KeyboardEvent) {
     if (
       e.metaKey ||
       e.altKey ||
@@ -212,39 +311,36 @@ class DocNavTreeController {
     ) {
       return;
     }
+
+    // KeyDown events should be within one second of each other to be considered
+    // part of the same typeahead search string.
     const MAX_TYPEAHEAD_THRESHOLD_MS = 1000;
     if (e.timeStamp - this.lastKeyDownTimeStamp > MAX_TYPEAHEAD_THRESHOLD_MS) {
       this.searchString = '';
     }
     this.lastKeyDownTimeStamp = e.timeStamp;
     this.searchString += e.key.toLocaleLowerCase();
-    const focusedElementText =
-      (_a = this.visibleItems[this.focusedIndex].textContent) === null || _a === void 0
-        ? void 0
-        : _a.toLocaleLowerCase();
-    if (
-      this.searchString.length === 1 ||
-      !(focusedElementText === null || focusedElementText === void 0
-        ? void 0
-        : focusedElementText.startsWith(this.searchString))
-    ) {
+    const focusedElementText = this.visibleItems[
+      this.focusedIndex
+    ].textContent?.toLocaleLowerCase();
+    if (this.searchString.length === 1 || !focusedElementText?.startsWith(this.searchString)) {
       this.focusNextItemWithPrefix(this.searchString);
     }
     e.stopPropagation();
     e.preventDefault();
   }
-  focusNextItemWithPrefix(prefix) {
-    var _a;
+
+  /**
+   * Focuses on the next visible tree item (after the currently focused element,
+   * wrapping the tree) that has a prefix equal to the given search string.
+   */
+  focusNextItemWithPrefix(prefix: string) {
     let i = this.focusedIndex + 1;
     if (i > this.visibleItems.length - 1) {
       i = 0;
     }
     while (i !== this.focusedIndex) {
-      if (
-        (_a = this.visibleItems[i].textContent) === null || _a === void 0
-          ? void 0
-          : _a.toLocaleLowerCase().startsWith(prefix)
-      ) {
+      if (this.visibleItems[i].textContent?.toLocaleLowerCase().startsWith(prefix)) {
         this.setFocusedIndex(i);
         return;
       }
@@ -255,95 +351,148 @@ class DocNavTreeController {
       }
     }
   }
-  toggleItemExpandedState(el) {
+
+  private toggleItemExpandedState(el: HTMLElement) {
     el.getAttribute('aria-expanded') === 'true' ? this.collapseItem(el) : this.expandItem(el);
   }
-  focusPreviousItem() {
+
+  private focusPreviousItem() {
     this.setFocusedIndex(Math.max(0, this.focusedIndex - 1));
   }
-  focusNextItem() {
+
+  private focusNextItem() {
     this.setFocusedIndex(Math.min(this.visibleItems.length - 1, this.focusedIndex + 1));
   }
-  collapseItem(el) {
+
+  private collapseItem(el: HTMLElement) {
     el.setAttribute('aria-expanded', 'false');
     this.updateVisibleItems();
   }
-  expandItem(el) {
+
+  private expandItem(el: HTMLElement) {
     el.setAttribute('aria-expanded', 'true');
     this.updateVisibleItems();
   }
-  focusParentItem(el) {
+
+  private focusParentItem(el: HTMLElement | null) {
     const owningItemEl = this.owningItem(el);
     if (owningItemEl) {
       this.setFocusedIndex(this.visibleItems.indexOf(owningItemEl));
     }
   }
-  owningItem(el) {
-    var _a;
-    const groupEl = el === null || el === void 0 ? void 0 : el.closest(`[role='group']`);
+
+  /**
+   * @returnThe first parent item that “owns” the group that el is a member of,
+   * or null if there is none.
+   */
+  owningItem(el: HTMLElement | null) {
+    const groupEl = el?.closest(`[role='group']`);
     if (!groupEl) {
       return null;
     }
-    return (_a = groupEl.parentElement) === null || _a === void 0
-      ? void 0
-      : _a.querySelector(`[aria-owns='${groupEl.id}']`);
+    return groupEl.parentElement?.querySelector<HTMLElement>(`[aria-owns='${groupEl.id}']`);
   }
-  updateVisibleItems() {
-    const allEls = Array.from(this.el.querySelectorAll(`[role='treeitem']`));
+
+  /**
+   * Updates which items are visible (not a child of a collapsed item).
+   */
+  private updateVisibleItems() {
+    const allEls = Array.from(this.el.querySelectorAll<HTMLElement>(`[role='treeitem']`));
     const hiddenEls = Array.from(
       this.el.querySelectorAll(`[aria-expanded='false'] + [role='group'] [role='treeitem']`)
     );
     this.visibleItems = allEls.filter(el => !hiddenEls.includes(el));
   }
 }
+
+/**
+ * Primary controller for the documentation page, handling coordination between
+ * the navigation and content components. This class ensures that any
+ * documentation elements in view are properly shown/highlighted in the
+ * navigation components.
+ *
+ * Since navigation is essentially handled by anchor tags with fragment IDs as
+ * hrefs, the fragment ID (referenced in this code as simply “ID”) is used to
+ * look up both navigation and content nodes.
+ */
 class DocPageController {
-  constructor(sideNavEl, mobileNavEl, contentEl) {
-    this.contentEl = contentEl;
+  private navController?: DocNavTreeController;
+  private mobileNavController?: MobileNavController;
+  /**
+   * Instantiates the controller, setting up the navigation controller (both
+   * desktop and mobile), and event listeners. This should only be called once.
+   */
+  constructor(
+    sideNavEl: HTMLElement | null,
+    mobileNavEl: HTMLElement | null,
+    private contentEl: HTMLElement | null
+  ) {
     if (!sideNavEl || !contentEl) {
       console.warn('Unable to find all elements needed for navigation');
       return;
     }
+
     this.navController = new DocNavTreeController(sideNavEl);
+
     if (mobileNavEl) {
       this.mobileNavController = new MobileNavController(mobileNavEl);
     }
     window.addEventListener('hashchange', () => this.handleHashChange());
+
     this.updateSelectedIdFromWindowHash();
   }
-  handleHashChange() {
+
+  /**
+   * Handles when the location hash changes.
+   */
+  private handleHashChange() {
     this.updateSelectedIdFromWindowHash();
   }
-  updateSelectedIdFromWindowHash() {
-    var _a, _b;
+
+  private updateSelectedIdFromWindowHash() {
     const targetId = this.targetIdFromLocationHash();
-    (_a = this.navController) === null || _a === void 0 ? void 0 : _a.setSelectedId(targetId);
+    this.navController?.setSelectedId(targetId);
     if (this.mobileNavController) {
       this.mobileNavController.setSelectedId(targetId);
     }
     if (targetId !== '') {
-      const targetEl =
-        (_b = this.contentEl) === null || _b === void 0
-          ? void 0
-          : _b.querySelector(`[id='${targetId}']`);
+      const targetEl = this.contentEl?.querySelector<HTMLElement>(`[id='${targetId}']`);
       if (targetEl) {
         targetEl.focus();
       }
     }
   }
+
   targetIdFromLocationHash() {
     return window.location.hash && window.location.hash.substr(1);
   }
 }
+
+/**
+ * Controller for the navigation element used on smaller viewports. It utilizes
+ * a native <select> element for interactivity and a styled <label> for
+ * displaying the selected option.
+ *
+ * It presumes a fixed header and that the container for the control will be
+ * sticky right below the header when scrolled enough.
+ */
 class MobileNavController {
-  constructor(el) {
-    var _a;
-    this.el = el;
-    this.selectEl = el.querySelector('select');
-    this.labelTextEl = el.querySelector('.js-mobileNavSelectText');
-    (_a = this.selectEl) === null || _a === void 0
-      ? void 0
-      : _a.addEventListener('change', e => this.handleSelectChange(e));
+  private selectEl: HTMLSelectElement | null;
+  private labelTextEl: HTMLElement | null;
+  private intersectionObserver: IntersectionObserver;
+
+  constructor(private el: HTMLElement) {
+    this.selectEl = el.querySelector<HTMLSelectElement>('select');
+    this.labelTextEl = el.querySelector<HTMLElement>('.js-mobileNavSelectText');
+
+    this.selectEl?.addEventListener('change', e => this.handleSelectChange(e));
+
+    // We use a slight hack to detect if the mobile nav container is pinned to
+    // the bottom of the site header. The root viewport of an IntersectionObserver
+    // is inset by the header height plus one pixel to ensure that the container is
+    // considered “out of view” when in a fixed position and can be styled appropriately.
     const ROOT_TOP_MARGIN = '-57px';
+
     this.intersectionObserver = new IntersectionObserver(
       entries => this.intersectionObserverCallback(entries),
       {
@@ -353,37 +502,40 @@ class MobileNavController {
     );
     this.intersectionObserver.observe(this.el);
   }
-  setSelectedId(id) {
+
+  setSelectedId(id: string) {
     if (!this.selectEl) return;
     this.selectEl.value = id;
     this.updateLabelText();
   }
-  updateLabelText() {
-    var _a;
+
+  private updateLabelText() {
     if (!this.labelTextEl || !this.selectEl) return;
-    const selectedIndex =
-      (_a = this.selectEl) === null || _a === void 0 ? void 0 : _a.selectedIndex;
+    const selectedIndex = this.selectEl?.selectedIndex;
     if (selectedIndex === -1) {
       this.labelTextEl.textContent = '';
       return;
     }
     this.labelTextEl.textContent = this.selectEl.options[selectedIndex].textContent;
   }
-  handleSelectChange(e) {
-    window.location.hash = `#${e.target.value}`;
+
+  private handleSelectChange(e: Event) {
+    window.location.hash = `#${(e.target as HTMLSelectElement).value}`;
     this.updateLabelText();
   }
-  intersectionObserverCallback(entries) {
+
+  private intersectionObserverCallback(entries: IntersectionObserverEntry[]) {
     const SHADOW_CSS_CLASS = 'DocNavMobile--withShadow';
     entries.forEach(entry => {
+      // entry.isIntersecting isn’t reliable on Firefox.
       const fullyInView = entry.intersectionRatio === 1.0;
       entry.target.classList.toggle(SHADOW_CSS_CLASS, !fullyInView);
     });
   }
 }
+
 new DocPageController(
   document.querySelector('.js-tree'),
   document.querySelector('.js-mobileNav'),
   document.querySelector('.js-unitDetailsContent')
 );
-//# sourceMappingURL=sidenav.js.map
