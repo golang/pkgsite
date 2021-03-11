b4399409b5ca78d7a846cd608c96bef4f14b4f16
Julie Qiu <julie@golang.org>
2021-03-11 13:30:21 -0500

    migrations: change package_symbols unique index
    The UNIQUE index for package_symbols is changed to
    (package_path_id, module_path_id, symbol_name_id, section, synopsis).
    
    Within a given package, a symbol's parent or type should not be able to
    change without the symbol's name, section, and/or synopsis changing.
    
    However, the section can change with the name and synopsis staying the
    same. For example, if `const Foo` is in the Constants Section, but is
    later modified such that it is of type T, it will now have parent T and
    and section Types, instead of parent Foo and section Constants, but the
    name will be Foo and synopsis will be `const Foo` in both cases.
    
    For golang/go#37102
    
    Change-Id: I3f8bb84d7376c1f9d0ebbd85c82329b4f27fa7f3
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/301050
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000079_change_package_symbol_unique.down.sql
@@ -0,0 +1,13 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+DROP INDEX package_symbols_package_path_id_module_path_id_symbol_name_id_synopsis_key;
+
+CREATE UNIQUE INDEX package_symbols_package_path_id_module_path_id_section_synopsis_key
+    ON package_symbols(
+        package_path_id, module_path_id, section, uuid_generate_v5(uuid_nil(), synopsis));
+
+END;
