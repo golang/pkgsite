2bc2ba4e81de61d1cff0e21436b1dd2d926e911c
Jamal Carvalho <jamal@golang.org>
2021-03-18 18:03:04 -0400

    e2e: add snapshot test for fixed header
    Change-Id: Ib7454175d68bd382709603575b089de8dc055fa9
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/303112
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- e2e/pkgsite.test.ts
+++ e2e/pkgsite.test.ts
@@ -6,12 +6,15 @@
  */
 
 import './globals';
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 
 const baseUrl = process.env.FRONTEND_URL ?? '';
 
 describe('golang.org/x/pkgsite', () => {
+  let page: Page;
+
   beforeAll(async () => {
+    page = await browser.newPage();
     await page.goto(baseUrl);
     await page.evaluate(() =>
       fetch(`/fetch/golang.org/x/pkgsite@v0.0.0-20210216165259-5867665b19ca`, { method: 'POST' })
@@ -19,7 +22,12 @@ describe('golang.org/x/pkgsite', () => {
   }, 30000);
 
   beforeEach(async () => {
+    page = await browser.newPage();
     await page.goto(baseUrl + '/golang.org/x/pkgsite');
+  });
+
+  afterEach(async () => {
+    await page.close();
   });
 
   test('accessibility tree matches snapshot', async () => {
@@ -35,6 +43,23 @@ describe('golang.org/x/pkgsite', () => {
   test('mobile viewport matches image snapshot', async () => {
     await page.emulate(puppeteer.devices['Pixel 2']);
     const image = await page.screenshot({ fullPage: true });
+    expect(image).toMatchImageSnapshot();
+  });
+
+  test('desktop fixed header appears after scrolling', async () => {
+    await page.mouse.wheel({ deltaY: 250 });
+    // wait for css transition
+    await page.waitForTimeout(500);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
+
+  test('mobile fixed header appears after scrolling', async () => {
+    await page.emulate(puppeteer.devices['Pixel 2']);
+    await page.mouse.wheel({ deltaY: 250 });
+    // wait for css transition
+    await page.waitForTimeout(500);
+    const image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
   });
 });
