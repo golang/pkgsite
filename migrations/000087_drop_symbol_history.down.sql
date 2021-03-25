96346d2cc7dbb24e4877fd408d8ee54f84d22cd0
Julie Qiu <julie@golang.org>
2021-03-25 13:10:19 -0400

    migrations: drop symbol_history table
    The symbol_history table is no longer being used and is dropped.
    
    Change-Id: Ib7b399869b7e46d37a727a3cea1c631ca218ab1f
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/304750
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000087_drop_symbol_history.down.sql
@@ -0,0 +1,27 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+CREATE TABLE symbol_history (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    package_path_id INTEGER NOT NULL,
+    module_path_id INTEGER NOT NULL,
+    symbol_id INTEGER NOT NULL,
+    parent_symbol_id INTEGER NOT NULL,
+    since_version TEXT NOT NULL,
+    section symbol_section NOT NULL,
+    synopsis text NOT NULL,
+    type symbol_type,
+    goos goos NOT NULL,
+    goarch goarch NOT NULL,
+
+    UNIQUE(package_path_id, module_path_id, symbol_id, goos, goarch),
+    FOREIGN KEY (parent_symbol_id) REFERENCES symbol_names(id) ON DELETE CASCADE,
+    FOREIGN KEY (module_path_id) REFERENCES paths(id) ON DELETE CASCADE,
+    FOREIGN KEY (symbol_id) REFERENCES symbol_names(id) ON DELETE CASCADE,
+    FOREIGN KEY (package_path_id) REFERENCES paths(id) ON DELETE CASCADE
+);
+
+END;
