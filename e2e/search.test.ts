1a3d58024bdb4be8b12a0c53a518d2dfc88f4278
Jamal Carvalho <jamal@golang.org>
2021-04-16 18:49:56 -0400

    e2e: update search visual tests
    The content on the search page varies too much to
    have a stable test. Updates the spec to replace
    variable content with constant values limiting
    the test to layout and style checks only.
    
    Change-Id: I107c925c99fbeeaf3325975420d32a23fd3b343b
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/310931
    Trust: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- e2e/search.test.ts
+++ e2e/search.test.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2020 The Go Authors. All rights reserved.
+ * Copyright 2021 The Go Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
@@ -8,33 +8,67 @@
 import './global-types';
 import puppeteer, { Page } from 'puppeteer';
 
-const baseUrl = process.env.FRONTEND_URL ?? '';
-
 describe('Search', () => {
   let page: Page;
 
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto(baseUrl + '/search?q=http');
+  beforeAll(async () => {
+    page = await newPage();
+    await page.goto(baseURL + '/search?q=http');
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await page.close();
   });
 
   test('accessibility tree matches snapshot', async () => {
+    await prepare(page);
     const a11yTree = await page.accessibility.snapshot();
     expect(a11yTree).toMatchSnapshot();
   });
 
   test('desktop viewport matches image snapshot', async () => {
+    await prepare(page);
     const image = await page.screenshot({ fullPage: true });
     expect(image).toMatchImageSnapshot();
   });
 
   test('mobile viewport matches image snapshot', async () => {
     await page.emulate(puppeteer.devices['Pixel 2']);
+    await prepare(page);
     const image = await page.screenshot({ fullPage: true });
     expect(image).toMatchImageSnapshot();
   });
 });
+
+/**
+ * prepare gets the page ready for snapshot tests by rewriting highly
+ * variable page content to constant values.
+ * @param page The page to prepare
+ */
+async function prepare(page: Page): Promise<void> {
+  await page.$$eval('[data-test-id="snippet-title"]', els =>
+    els.map(el => {
+      el.innerHTML = 'net/http/pprof';
+      (el as HTMLAnchorElement).href = 'net/http/pprof';
+    })
+  );
+  await page.$$eval('[data-test-id="snippet-synopsis"]', els =>
+    els.map(
+      el =>
+        (el.innerHTML =
+          'Package pprof serves via its HTTP server runtime profiling data in the format expected by the pprof visualization tool.')
+    )
+  );
+  await page.$$eval('[data-test-id="snippet-version"]', els =>
+    els.map(el => (el.innerHTML = 'go1.16.3'))
+  );
+  await page.$$eval('[data-test-id="snippet-published"]', els =>
+    els.map(el => (el.innerHTML = 'Apr 1, 2021'))
+  );
+  await page.$$eval('[data-test-id="snippet-importedby"]', els =>
+    els.map(el => (el.innerHTML = '11632'))
+  );
+  await page.$$eval('[data-test-id="snippet-license"]', els =>
+    els.map(el => (el.innerHTML = 'BSD-3-Clause'))
+  );
+}
