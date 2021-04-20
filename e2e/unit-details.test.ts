13a365b69466dd73d5783969a4f6905317e54b0a
Jamal Carvalho <jamal@golang.org>
2021-04-20 16:05:29 -0400

    e2e: additional unit details snapshot tests
    Adds snapshot tests for various features on the unit page.
    - accessibility tree
    - readme expand and collapse
    - directories expand and collapse
    - jump to without identifiers
    - jump to identifier
    - documentation outline
    
    Change-Id: I491b6305fb09923ed65db4db87622e9452a06420
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/311970
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Julie Qiu <julie@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>

--- e2e/unit-details.test.ts
+++ e2e/unit-details.test.ts
@@ -20,17 +20,53 @@ describe('Unit Details - golang.org/x/pkgsite - desktop', () => {
     await page.close();
   });
 
+  test('accessibility tree matches snapshot', async () => {
+    const a11yTree = await page.accessibility.snapshot();
+    expect(a11yTree).toMatchSnapshot();
+  });
+
   test('viewport matches image snapshot', async () => {
     const image = await page.screenshot({ fullPage: true });
     expect(image).toMatchImageSnapshot({});
   });
 
   test('fixed header appears after scrolling', async () => {
-    await page.mouse.wheel({ deltaY: 250 });
-    // wait for css transition
-    await page.waitForTimeout(500);
+    await page.evaluate(() => window.scrollTo({ top: 250 }));
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
+  });
+
+  test('readme expand and collapse', async () => {
+    await page.click(select('readme-expand'));
+    await page.evaluate(() => window.scrollTo({ top: 0 }));
+    const expanded = await page.screenshot({ fullPage: true });
+    expect(expanded).toMatchImageSnapshot();
+    await page.click(select('readme-collapse'));
+    await page.evaluate(() => window.scrollTo({ top: 0 }));
+    const collapsed = await page.screenshot({ fullPage: true });
+    expect(collapsed).toMatchImageSnapshot();
+  });
+
+  test('directories expand and collapse', async () => {
+    await page.click(select('directories-toggle'));
+    await page.evaluate(() => window.scrollTo({ top: 0 }));
+    const expanded = await page.screenshot({ fullPage: true });
+    expect(expanded).toMatchImageSnapshot();
+    await page.click(select('directories-toggle'));
+    await page.evaluate(() => window.scrollTo({ top: 0 }));
+    const collapsed = await page.screenshot({ fullPage: true });
+    expect(collapsed).toMatchImageSnapshot();
+  });
+
+  test('jump to without identifiers', async () => {
+    await page.click(select('jump-to-button'));
+    await page.evaluate(() => window.scrollTo({ top: 0 }));
+    const expanded = await page.screenshot();
+    expect(expanded).toMatchImageSnapshot();
+    await page.click(select('close-dialog'));
+    await page.evaluate(() => window.scrollTo({ top: 0 }));
+    const collapsed = await page.screenshot();
+    expect(collapsed).toMatchImageSnapshot();
   });
 });
 
@@ -47,17 +83,74 @@ describe('Unit Details - golang.org/x/pkgsite - mobile', () => {
     await page.close();
   });
 
+  test('accessibility tree matches snapshot', async () => {
+    const a11yTree = await page.accessibility.snapshot();
+    expect(a11yTree).toMatchSnapshot();
+  });
+
   test('viewport matches image snapshot', async () => {
     const image = await page.screenshot({ fullPage: true });
     expect(image).toMatchImageSnapshot();
   });
 
   test('fixed header appears after scrolling', async () => {
-    await page.mouse.wheel({ deltaY: 250 });
-    // wait for css transition
-    await page.waitForTimeout(500);
+    await page.evaluate(() => window.scrollTo({ top: 250 }));
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
+  });
+});
+
+describe('Unit Details - golang.org/x/pkgsite/internal/derrors', () => {
+  let page: Page;
+  beforeAll(async () => {
+    page = await newPage();
+    await page.goto(baseURL + '/golang.org/x/pkgsite/internal/derrors');
+    await prepare(page);
+  });
+
+  afterAll(async () => {
+    await page.close();
+  });
+
+  test('accessibility tree matches snapshot', async () => {
+    const a11yTree = await page.accessibility.snapshot();
+    expect(a11yTree).toMatchSnapshot();
+  });
+
+  test('viewport matches image snapshot', async () => {
+    const image = await page.screenshot({ fullPage: true });
+    expect(image).toMatchImageSnapshot();
+  });
+
+  test.each`
+    href
+    ${'#section-documentation'}
+    ${'#pkg-overview'}
+    ${'#pkg-index'}
+    ${'#pkg-constants'}
+    ${'#pkg-variables'}
+    ${'#pkg-functions'}
+    ${'#Add'}
+    ${'#pkg-types'}
+    ${'#StackError'}
+    ${'#NewStackError'}
+    ${'#section-sourcefiles'}
+  `('documentation outline $href', async ({ href }) => {
+    await page.click(`[href="${href}"][role="treeitem"]`);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
+
+  test('jump to with identifiers', async () => {
+    await page.click(select('jump-to-button'));
+    const expanded = await page.screenshot();
+    expect(expanded).toMatchImageSnapshot();
+    await page.keyboard.type('Wrap');
+    const inputWrap = await page.screenshot();
+    expect(inputWrap).toMatchImageSnapshot();
+    await page.keyboard.press('Enter');
+    const wrap = await page.screenshot();
+    expect(wrap).toMatchImageSnapshot();
   });
 });
 
@@ -66,24 +159,47 @@ test('no page errors', () => {
 });
 
 /**
+ * select will create a data-test-id attribute selector for a given test id.
+ * @param testId the test id of the element to select.
+ * @param rest a place to add combinators and additional selectors.
+ * @returns an attribute selector.
+ */
+function select(testId: string, rest = ''): string {
+  return `[data-test-id="${testId}"] ${rest}`;
+}
+
+/**
  * prepare gets the page ready for snapshot testing by rewriting highly
  * variable page content to constant values.
  * @param page The page to prepare
  */
 async function prepare(page: Page): Promise<void> {
   await Promise.all([
+    // Add styles to disable animation transitions.
+    page.addStyleTag({
+      content: `
+        *,
+        *::after,
+        *::before {
+            transition-delay: 0s !important;
+            transition-duration: 0s !important;
+            animation-delay: -0.0001s !important;
+            animation-duration: 0s !important;
+            animation-play-state: paused !important;
+        }`,
+    }),
     page.$eval(
-      '[data-test-id="UnitHeader-version"] a',
+      select('UnitHeader-version', 'a'),
       el => ((el as HTMLElement).innerHTML = '<span>Version: </span>v0.0.0')
     ),
     page.$eval(
-      '[data-test-id="UnitHeader-commitTime"]',
+      select('UnitHeader-commitTime'),
       el => ((el as HTMLElement).innerHTML = 'Published: Apr 16, 2021')
     ),
-    page.$$eval('[data-test-id="UnitHeader-imports"] a', els =>
+    page.$$eval(select('UnitHeader-imports', 'a'), els =>
       els.map(el => (el.innerHTML = 'Imports: 0'))
     ),
-    page.$$eval('[data-test-id="UnitHeader-importedby"] a', els =>
+    page.$$eval(select('UnitHeader-importedby', 'a'), els =>
       els.map(el => (el.innerHTML = 'Imported by: 0'))
     ),
   ]);
