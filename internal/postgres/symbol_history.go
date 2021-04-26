6edacfa59a0e3dbd47a11808b4c322ffbca180b6
Julie Qiu <julie@golang.org>
2021-04-26 16:02:18 -0400

    internal/postgres: add getSymbolHistory
    getSymbolHistory is added, which reads data from the symbol_history
    table and returns information on the first version that symbol was added
    to a package.
    
    Tests are added in CL 313596.
    
    For golang/go#37102
    
    Change-Id: I4ad5de1d6f9ea8c674732c62d192276dd3533df4
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/313595
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- internal/postgres/symbol_history.go
+++ internal/postgres/symbol_history.go
@@ -6,8 +6,11 @@ package postgres
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 
 	"golang.org/x/pkgsite/internal"
+	"golang.org/x/pkgsite/internal/database"
 	"golang.org/x/pkgsite/internal/derrors"
 	"golang.org/x/pkgsite/internal/experiment"
 	"golang.org/x/pkgsite/internal/middleware"
@@ -24,14 +27,75 @@ func (db *DB) GetSymbolHistory(ctx context.Context, packagePath, modulePath stri
 	defer middleware.ElapsedStat(ctx, "GetSymbolHistory")()
 
 	if experiment.IsActive(ctx, internal.ExperimentReadSymbolHistory) {
-		// TODO(https://golang.org/issue/37102): read data from the
-		// symbol_history table.
-		return nil, nil
+		return getSymbolHistory(ctx, db.db, packagePath, modulePath)
 	}
-
 	versionToNameToUnitSymbols, err := getPackageSymbols(ctx, db.db, packagePath, modulePath)
 	if err != nil {
 		return nil, err
 	}
 	return symbol.IntroducedHistory(versionToNameToUnitSymbols), nil
+}
+
+// getSymbolHistory fetches symbol history data from the symbol_history table.
+func getSymbolHistory(ctx context.Context, ddb *database.DB, packagePath, modulePath string,
+) (_ map[string]map[string]*internal.UnitSymbol, err error) {
+	defer derrors.WrapStack(&err, "getSymbolHistory(ctx, ddb, %q, %q)", packagePath, modulePath)
+	query := `
+		SELECT
+			s1.name AS symbol_name,
+			s2.name AS parent_symbol_name,
+			ps.section,
+			ps.type,
+			ps.synopsis,
+			sh.since_version,
+			sh.goos,
+			sh.goarch
+		FROM
+			symbol_history sh
+		INNER JOIN package_symbols ps ON ps.id = sh.package_symbol_id
+		INNER JOIN symbol_names s1 ON ps.symbol_name_id = s1.id
+		INNER JOIN symbol_names s2 ON ps.parent_symbol_name_id = s2.id
+		INNER JOIN paths p1 ON sh.package_path_id = p1.id
+		INNER JOIN paths p2 ON sh.module_path_id = p2.id
+		WHERE
+			p1.path = $1
+			AND p2.path = $2;`
+
+	// versionToNameToUnitSymbol is a map of the version a symbol was
+	// introduced, to the name and unit symbol.
+	versionToNameToUnitSymbol := map[string]map[string]*internal.UnitSymbol{}
+	collect := func(rows *sql.Rows) error {
+		var (
+			newUS internal.UnitSymbol
+			build internal.BuildContext
+		)
+		if err := rows.Scan(
+			&newUS.Name,
+			&newUS.ParentName,
+			&newUS.Section,
+			&newUS.Kind,
+			&newUS.Synopsis,
+			&newUS.Version,
+			&build.GOOS,
+			&build.GOARCH,
+		); err != nil {
+			return fmt.Errorf("row.Scan(): %v", err)
+		}
+		nts, ok := versionToNameToUnitSymbol[newUS.Version]
+		if !ok {
+			nts = map[string]*internal.UnitSymbol{}
+			versionToNameToUnitSymbol[newUS.Version] = nts
+		}
+		us, ok := nts[newUS.Name]
+		if !ok {
+			us = &newUS
+			nts[newUS.Name] = us
+		}
+		us.AddBuildContext(build)
+		return nil
+	}
+	if err := ddb.RunQuery(ctx, query, collect, packagePath, modulePath); err != nil {
+		return nil, err
+	}
+	return versionToNameToUnitSymbol, nil
 }
