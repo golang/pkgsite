bf8ef1cf544a1a04f8cd34488e0d7c9855591e36
Julie Qiu <julie@golang.org>
2021-04-27 23:03:49 -0400

    internal/testing/integration: add TestSymbols
    An integration test is added which fetches all versions of a module from
    the proxy, parses the frontend details page, and compares that output to
    a golden dataset in internal/symbol/testdata.
    
    For golang/go#37102
    
    Change-Id: Ie1507534bb6db65031d1e69f91f22287fb52415c
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/314538
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ internal/testing/integration/frontend_symbol_test.go
@@ -0,0 +1,89 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package integration
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"golang.org/x/pkgsite/internal"
+	"golang.org/x/pkgsite/internal/experiment"
+	"golang.org/x/pkgsite/internal/frontend"
+	"golang.org/x/pkgsite/internal/postgres"
+	"golang.org/x/pkgsite/internal/symbol"
+)
+
+func TestSymbols(t *testing.T) {
+	defer postgres.ResetTestDB(testDB, t)
+	for _, exp := range []struct {
+		name string
+		exps []string
+	}{
+		{
+			"no experiment",
+			[]string{
+				internal.ExperimentSymbolHistoryVersionsPage,
+			},
+		},
+		{
+			"experiment insert and read symbol_history",
+			[]string{
+				internal.ExperimentInsertSymbolHistory,
+				internal.ExperimentReadSymbolHistory,
+				internal.ExperimentSymbolHistoryVersionsPage,
+			},
+		},
+	} {
+		t.Run(exp.name, func(t *testing.T) {
+			exps := exp.exps
+			processVersions(
+				experiment.NewContext(context.Background(), exps...),
+				t, testModules)
+
+			for _, test := range []struct {
+				name, pkgPath, modulePath string
+			}{
+				{
+					"test v1 module (rsc.io quote)",
+					"rsc.io/quote",
+					"rsc.io/quote",
+				},
+			} {
+				t.Run(test.name, func(t *testing.T) {
+					// Get api data.
+					files, err := symbol.LoadAPIFiles(test.pkgPath, "../../symbol/testdata")
+					if err != nil {
+						t.Fatal(err)
+					}
+					apiVersions, err := symbol.ParsePackageAPIInfo(files)
+					if err != nil {
+						t.Fatal(err)
+					}
+
+					// Get frontend data.
+					urlPath := fmt.Sprintf("/%s?tab=versions&m=json", test.pkgPath)
+					body := getFrontendPage(t, urlPath, exps...)
+					var vd frontend.VersionsDetails
+					if err := json.Unmarshal([]byte(body), &vd); err != nil {
+						t.Fatalf("json.Unmarshal: %v\n %s", err, body)
+					}
+					versionToNameToSymbol, err := frontend.ParseVersionsDetails(vd)
+					if err != nil {
+						t.Fatal(err)
+					}
+
+					// Compare the output of these two data sources.
+					errs := symbol.CompareAPIVersions(test.pkgPath,
+						apiVersions[test.pkgPath], versionToNameToSymbol)
+					for _, e := range errs {
+						t.Error(e)
+					}
+				})
+			}
+		})
+	}
+}
