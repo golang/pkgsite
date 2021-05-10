c31e9966f1d07f6c833f95af421b328121bfc92b
Julie Qiu <julie@golang.org>
2021-05-10 14:33:43 -0400

    migrations: add search_documents package_path_id and module_path_id
    For golang/go#44142
    
    Change-Id: I9d1908b07924a02153a249773d9440676a0e349a
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/318489
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ migrations/000097_alter_search_documents.up.sql
@@ -0,0 +1,10 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE search_documents RENAME path_id TO package_path_id;
+ALTER TABLE search_documents ADD COLUMN module_path_id INTEGER;
+
+END;
