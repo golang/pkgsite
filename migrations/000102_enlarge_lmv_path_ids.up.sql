396b595e0aebaecca63435fe1b4ad494d46cf662
Jonathan Amsterdam <jba@google.com>
2021-05-24 12:09:18 -0400

    migrations: change type of latest_module_versions.module_path_id to bigint
    First of a series of migrations that will enlarge the types of columns
    that refer to paths.id.
    
    We'll do each table separately because they can take over an hour.
    
    Change-Id: Iff254e0d8f93d3e3bd49685ea355d525e381eba1
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/322170
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>

---
+++ migrations/000102_enlarge_lmv_path_ids.up.sql
@@ -0,0 +1,9 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE latest_module_versions ALTER COLUMN module_path_id TYPE BIGINT;
+
+END;
