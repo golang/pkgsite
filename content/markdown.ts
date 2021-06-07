69d60f218d9607225eaa005309ad1424148477ef
Jamal Carvalho <jamal@golang.org>
2021-06-07 15:30:19 -0400

    content: add documentation for mardown parser function
    Change-Id: Iade3e7ba45bc9662dbffdb07d6a062fd7cd862ff
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/325854
    Trust: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- content/markdown.ts
+++ content/markdown.ts
@@ -1,6 +1,13 @@
 import marked from 'marked';
 import fs from 'fs/promises';
 
+/**
+ * parse extracts code snippets from markdown files in component
+ * directories for use as html in unit tests.
+ * @param file path to a markdown file.
+ * @returns code snippet from markdown file suitable to use
+ * in static unit tests.
+ */
 export async function parse(file: string): Promise<string> {
   marked.use({ renderer: { code: code => code } });
   return marked(await fs.readFile(file, { encoding: 'utf-8' }));
