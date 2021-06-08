925374439821fe61fd16f10adcda86f857db4ea7
Jonathan Amsterdam <jba@google.com>
2021-06-08 13:48:11 -0400

    migrations: validate foreign key constraint on documentation.unit_id
    Change-Id: I3d484f89624b4e169fecfedaea5348c7b2802192
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/326129
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

---
+++ migrations/000112_validate_documentation_fkey_constraint.up.sql
@@ -0,0 +1,5 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+ALTER TABLE documentation VALIDATE CONSTRAINT documentation_big_unit_id_fkey;
