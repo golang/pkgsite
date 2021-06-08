3fbd623fd94799adec91ed1de86c40f3bf6e462c
Jonathan Amsterdam <jba@google.com>
2021-06-07 22:52:16 -0400

    migrations: add foreign key constraint for documentation.big_unit_id
    Adding this constraint required pausing the queue to finish without
    error, so it's in a separate migration from adding the column itself.
    
    Change-Id: Ic3a42f3c962042e6bb4c49056d7e5cbb75d747ed
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/325705
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

---
+++ migrations/000110_add_doc_fkey_constraint.up.sql
@@ -0,0 +1,11 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE documentation ADD CONSTRAINT "documentation_big_unit_id_fkey"
+	FOREIGN KEY (big_unit_id) REFERENCES units(id) ON DELETE CASCADE
+	NOT VALID;
+
+END;
