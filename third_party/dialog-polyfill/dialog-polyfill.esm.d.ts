abc2686366fa6badb18319c8017f9047022c7bb2
Jamal Carvalho <jamal@golang.org>
2021-06-09 21:52:56 -0400

    third_party: add type definition for dialog polyfill
    Change-Id: I89c586cc3b0d115357f758d32f885d08ac64bf35
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/326709
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Trust: Jamal Carvalho <jamal@golang.org>

---
+++ third_party/dialog-polyfill/dialog-polyfill.esm.d.ts
@@ -0,0 +1,17 @@
+interface DialogPolyfillType {
+  registerDialog(dialog: HTMLDialogElement): void;
+  forceRegisterDialog(dialog: HTMLDialogElement): void;
+}
+
+/**
+ * If used as CJS, then "dialogPolyfill" is added to the global scope. Just assert it exists.
+ */
+declare global {
+  const dialogPolyfill: DialogPolyfillType;
+}
+
+/**
+ * If used as ESM, then we export a const of type "DialogPolyfillType" as the default.
+ */
+declare const dialogPolyfill: DialogPolyfillType
+export default dialogPolyfill;
