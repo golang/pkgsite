d453543931a946f1767dea840400eeea4e9cd869
Jamal Carvalho <jamal@golang.org>
2021-06-11 19:26:22 -0400

    content: remove unused content directory
    The files from content/static have moved to static/legacy.
    
    Change-Id: If733c4df8a7b964527eb178fbdee414f1914206e
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/327284
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

--- content/static/worker/worker.js
+++
@@ -1,7 +0,0 @@
-/*!
- * @license
- * Copyright 2021 The Go Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
- */function submitForm(o,s){const e=document.querySelector(`form[name="${o}" ]`);if(!e)throw Error(`Form "${o}" not found.`);e.result.value="request pending...";const t=new XMLHttpRequest;t.onreadystatechange=function(){this.readyState==4&&(this.status>=200&&this.status<300?s?location.reload():e.result.value="Success.":e.result.value="ERROR: "+this.responseText)},t.open(e.method,e.action),t.send(new FormData(e))}window.submitForm=submitForm;
-//# sourceMappingURL=worker.js.map
