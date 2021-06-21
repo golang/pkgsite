6eaddae167b21bac385ef1add18f48e2690fae4c
Julie Qiu <julie@golang.org>
2021-06-21 16:12:42 -0400

    devtools: use docker-compose nodejs in nodejs.sh
    Now that we can use the nodejs in the docker-compose file for running
    e2e tests against different environments, switch back to using the
    nodejs set up using docker-compose.
    
    This reverts commit bd83a0e9ec81216a0dd3c749ce7a98fc7a18ffb7.
    
    Change-Id: Ia19e3415b85cd4ff726e80913bc9e0a4cdb095e4
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/329916
    Trust: Julie Qiu <julie@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- devtools/nodejs.sh
+++ devtools/nodejs.sh
@@ -10,13 +10,4 @@ set -e
 # It passes env variables for e2e tests,
 # mounts the pwd into a volume in the container at /pkgsite,
 # and sets the working directory in the container to /pkgsite.
-
-docker run --net=host --rm -t \
-  -e CI \
-  -e GO_DISCOVERY_E2E_BASE_URL \
-  -e GO_DISCOVERY_E2E_AUTHORIZATION \
-  -e GO_DISCOVERY_E2E_QUOTA_BYPASS \
-  -e PUPPETEER_SKIP_CHROMIUM_DOWNLOAD \
-  -v `pwd`:/pkgsite \
-  -w /pkgsite  \
-  node:14.17.0 $@
+docker-compose -f devtools/docker/docker-compose.yaml run nodejs $@
