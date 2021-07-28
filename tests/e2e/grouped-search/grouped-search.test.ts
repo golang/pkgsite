85c15feb55ffa5892a68f30a9013947d2bb2aef0
Jamal Carvalho <jamal@golang.org>
2021-07-28 15:12:46 -0400

    tests/e2e/grouped-search: add example e2e test feature directory
    This CL demonstrates the usage of feature directories
    for the e2e tests. These tests will not run in CI.
    
    Change-Id: I49266bca846e79d1bacc16491da30366baa18044
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/338114
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

---
+++ tests/e2e/grouped-search/grouped-search.test.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright 2020 The Go Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+import * as pg from '../helpers/page';
+import * as search from '../helpers/search.page';
+
+test('screenshot', async () => {
+  const page = await pg.newPage();
+  await pg.fullScreenshotTest(page, {
+    path: '/search?q=http',
+    prepare: search.prepare,
+  });
+});
+
+test('no page errors', () => {
+  expect(pageErrors).toHaveLength(0);
+});
