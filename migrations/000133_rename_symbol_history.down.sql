8309cc4fe61473b650f908e4d9665587ab1c3a2a
Jonathan Amsterdam <jba@google.com>
2021-07-30 10:15:25 -0400

    migrations: rename new_symbol_history to symbol_history
    Change-Id: Ia6c19f80c3e39940daa590e1c8043b6b8a124708
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/338572
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

---
+++ migrations/000133_rename_symbol_history.down.sql
@@ -0,0 +1,10 @@
+-- Copyright 2021 The Go Authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+BEGIN;
+
+ALTER TABLE symbol_history RENAME TO new_symbol_history;
+ALTER TABLE old_symbol_history RENAME TO symbol_history;
+
+END;
