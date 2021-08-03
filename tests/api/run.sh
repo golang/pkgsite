6c204a9302a3a50393bb90b58217473fcfbaecbd
Julie Qiu <julie@golang.org>
2021-08-03 12:34:40 -0400

    devtools,tests/api: add set up with docker
    tests/api/run.sh is added, which runs the symbol history comparison
    script in docker.
    
    For golang/go#37102
    
    Change-Id: I11d7f3a61277d6756308f854a78504c3b63ac4fd
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/339395
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ tests/api/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+source devtools/docker.sh || { echo "Are you at repo root?"; exit 1; }
+
+set -e
+
+main() {
+  trap docker_cleanup EXIT
+  trap docker_error ERR
+
+  export GO_DISCOVERY_DATABASE_NAME=discovery_api_test
+  export GO_DISCOVERY_CONFIG_DYNAMIC=tests/api/config.yaml
+  export GO_DISCOVERY_SEED_DB_FILE=tests/api/seed.txt
+  dockercompose build && dockercompose run seeddb && dockercompose run api
+
+  local status=$?
+  if [ $status -eq 0 ]
+  then
+    echo "Done!"
+  else
+    echo "API tests failed."
+  fi
+  exit $status
+}
+
+main $@
