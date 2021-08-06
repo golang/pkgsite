3087db9baa13febb46bb4cb3cd6786203a3446ec
Jamal Carvalho <jamal@golang.org>
2021-08-06 16:02:30 -0400

    static/{frontend,shared}: remove search submit on mode change
    Switching the type of search with a single click will be
    available as a text link on the search page. The dropdown
    in the header no longer resubmits the current query in the
    selected mode.
    
    For golang/go#47321
    
    Change-Id: Ie741a7c0d6b59298debcb27d9be65b864d6acb83
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/340474
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

--- static/shared/header/header.ts
+++ static/shared/header/header.ts
@@ -40,24 +40,5 @@ function registerSearchFormListeners() {
   });
 }
 
-/**
- * Listen for changes in the search dropdown.
- *
- * TODO(https://golang.org/issue/44142): Fix this flow:
- * A user will likely expect to submit the search again after selecting the
- * type of search. The change event should trigger a form submission, so that the
- * search event is still captured in analytics without a manual instrumentation.
- */
-document.querySelectorAll('.js-searchModeSelect').forEach(el => {
-  el.addEventListener('change', e => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-    const query = params['q'];
-    if (query) {
-      window.location.search = `q=${query}&m=${(e.target as HTMLSelectElement).value}`;
-    }
-  });
-});
-
 registerHeaderListeners();
 registerSearchFormListeners();
