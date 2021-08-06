ac09b70a4c87d302b0e7f66aadb251b659deed37
Jamal Carvalho <jamal@golang.org>
2021-08-06 16:20:34 -0400

    tests/e2e/grouped-search: update grouped search snapshots
    Change-Id: Ibc4a77375f8e70dde2993952e73f561d679a4775
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/340477
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Julie Qiu <julie@golang.org>

--- tests/e2e/grouped-search/grouped-search.test.ts
+++ tests/e2e/grouped-search/grouped-search.test.ts
@@ -8,10 +8,34 @@
 import * as pg from '../helpers/page';
 import * as search from '../helpers/search.page';
 
-test('screenshot', async () => {
+test('package: http', async () => {
   const page = await pg.newPage();
   await pg.fullScreenshotTest(page, {
     path: '/search?q=http',
+    prepare: search.prepare,
+  });
+});
+
+test('package: no results', async () => {
+  const page = await pg.newPage();
+  await pg.fullScreenshotTest(page, {
+    path: '/search?q=zhttpz',
+    prepare: search.prepare,
+  });
+});
+
+test('identifier: context', async () => {
+  const page = await pg.newPage();
+  await pg.fullScreenshotTest(page, {
+    path: '/search?m=identifiers&q=context',
+    prepare: search.prepare,
+  });
+});
+
+test('identifier: no results', async () => {
+  const page = await pg.newPage();
+  await pg.fullScreenshotTest(page, {
+    path: '/search?m=identifiers&q=zhttpz',
     prepare: search.prepare,
   });
 });
