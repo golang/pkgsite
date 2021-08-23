25e51bd952c2f3cdc94bc3ae8d0f98721fe486b5
Jamal Carvalho <jamal@golang.org>
2021-08-23 17:09:53 +0000

    static: regenerate minified assetss
    Change-Id: I2878b6189ce0b1c18a8a0e74bf2227afe5d70c0e
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/344400
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- static/shared/analytics/analytics.js
+++ static/shared/analytics/analytics.js
@@ -1,2 +1,2 @@
-export function track(n,a,t,e){window.dataLayer??=[],typeof n=="string"?window.dataLayer.push({event:n,event_category:a,event_action:t,event_label:e}):window.dataLayer.push(n)}export function func(n){window.dataLayer??=[],window.dataLayer.push(n)}
+export function track(n,a,e,r){var t;(t=window.dataLayer)!=null||(window.dataLayer=[]),typeof n=="string"?window.dataLayer.push({event:n,event_category:a,event_action:e,event_label:r}):window.dataLayer.push(n)}export function func(n){var a;(a=window.dataLayer)!=null||(window.dataLayer=[]),window.dataLayer.push(n)}
 //# sourceMappingURL=analytics.js.map
--- static/shared/modal/modal.js
+++ static/shared/modal/modal.js
@@ -3,5 +3,5 @@
  * Copyright 2021 The Go Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
- */export class ModalController{constructor(e){this.el=e;!window.HTMLDialogElement&&!e.showModal&&import("../../../third_party/dialog-polyfill/dialog-polyfill.esm.js").then(({default:l})=>{l.registerDialog(e)});const t=e.id,o=document.querySelector(`[aria-controls="${t}"]`);o&&o.addEventListener("click",()=>{this.el.showModal?this.el.showModal():this.el.open=!0,e.querySelector("input")?.focus()});for(const l of this.el.querySelectorAll("[data-modal-close]"))l.addEventListener("click",()=>{this.el.close?this.el.close():this.el.open=!1})}}
+ */export class ModalController{constructor(l){this.el=l;!window.HTMLDialogElement&&!l.showModal&&import("../../../third_party/dialog-polyfill/dialog-polyfill.esm.js").then(({default:e})=>{e.registerDialog(l)});const t=l.id,o=document.querySelector(`[aria-controls="${t}"]`);o&&o.addEventListener("click",()=>{var e;this.el.showModal?this.el.showModal():this.el.open=!0,(e=l.querySelector("input"))==null||e.focus()});for(const e of this.el.querySelectorAll("[data-modal-close]"))e.addEventListener("click",()=>{this.el.close?this.el.close():this.el.open=!1})}}
 //# sourceMappingURL=modal.js.map
