bbe22f36071f4fb11b28d57fb27209086981a847
Jamal Carvalho <jamal@golang.org>
2022-01-31 15:58:25 +0000

    deploy: replace e2e tests with screentest
    Replaced the e2e check in the deploy.yaml config.
    
    Change-Id: I9cd3e2beed9b729bbfe5f28f049ea848b1ad8976
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/382034
    Trust: Jamal Carvalho <jamal@golang.org>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

---
+++ deploy/screentest.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+# Copyright 2021 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+source private/devtools/lib.sh || { echo "Are you at repo root?"; exit 1; }
+
+usage() {
+  >&2 cat <<EOUSAGE
+
+  Usage: $0 [exp|dev|staging|prod|beta] IDTOKEN
+
+  Run the screentest check against a live instance of the given environment.
+  These tests will only pass against staging and prod.
+
+EOUSAGE
+  exit 1
+}
+
+main() {
+  local env=$1
+  local idtoken=$2
+  check_env $env
+  if [ -z $idtoken ]; then
+    idtoken=$(cat _ID_TOKEN)
+  fi
+  ./tests/screentest/run.sh --idtoken $idtoken $env
+}
+
+main $@
