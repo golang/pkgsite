0007cf85bfaa61fe68e207c8cf7adf11fbd23f97
Jonathan Amsterdam <jba@google.com>
2022-02-11 14:40:58 -0500

    internal/godoc/internal/doc: mark comparable as predeclared
    Fixes golang/go#51141.
    
    Change-Id: Id92108ff3c561ab33a119c395cbad09555b864f5
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/385198
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- internal/godoc/internal/doc/reader.go
+++ internal/godoc/internal/doc/reader.go
@@ -928,6 +928,7 @@ var predeclaredTypes = map[string]bool{
 	"any":        true,
 	"bool":       true,
 	"byte":       true,
+	"comparable": true,
 	"complex64":  true,
 	"complex128": true,
 	"error":      true,
