e388c1678c2f85e12c2e88a05c71ab90039edf50
Conor Britain <conorbritain@gmail.com>
2022-08-03 14:08:55 -0700

    static/frontend: update navigation to match go.dev
    Adds dropdown menus as well as fixed open/closed states. Adds submenus
    to mobile header nav.
    
    Change-Id: I952be1a8cc6fac70669ad6fd6a161ec9d2c72cb5
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/421514
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>

--- static/shared/header/header.ts
+++ static/shared/header/header.ts
@@ -6,24 +6,213 @@
  */
 
 export function registerHeaderListeners(): void {
-  const header = document.querySelector('.js-header');
-  const menuButtons = document.querySelectorAll('.js-headerMenuButton');
-  menuButtons.forEach(button => {
+  const header = document.querySelector('.js-header') as HTMLElement;
+
+  // Desktop menu hover state
+  const menuItemHovers = document.querySelectorAll('.js-desktop-menu-hover');
+  menuItemHovers.forEach(menuItemHover => {
+    // when user clicks on the dropdown menu item on desktop or mobile,
+    // force the menu to stay open until the user clicks off of it.
+    menuItemHover.addEventListener('mouseenter', e => {
+      const target = e.target as HTMLElement;
+      const forced = document.querySelector('.forced-open') as HTMLElement;
+      if (forced && forced !== menuItemHover) {
+        forced.blur();
+        forced.classList.remove('forced-open');
+      }
+      // prevents menus that have been tabbed into from staying open
+      // when you hover over another menu
+      target.focus();
+      target.blur();
+    });
+
+    const toggleForcedOpen = (e: Event) => {
+      const target = e.target as HTMLElement;
+      const isForced = target?.classList.contains('forced-open');
+      const currentTarget = e.currentTarget as HTMLElement;
+      if (isForced) {
+        currentTarget.removeEventListener('blur', () =>
+          currentTarget.classList.remove('forced-open')
+        );
+        currentTarget.classList.remove('forced-open');
+        currentTarget.classList.add('forced-closed');
+        currentTarget.blur();
+        currentTarget?.parentNode?.addEventListener('mouseout', () => {
+          currentTarget.classList.remove('forced-closed');
+        });
+      } else {
+        currentTarget.classList.remove('forced-closed');
+        currentTarget.classList.add('forced-open');
+        currentTarget.focus();
+        currentTarget.addEventListener('blur', () => currentTarget.classList.remove('forced-open'));
+        currentTarget?.parentNode?.removeEventListener('mouseout', () => {
+          currentTarget.classList.remove('forced-closed');
+        });
+      }
+    };
+    menuItemHover.addEventListener('click', toggleForcedOpen);
+  });
+
+  // ensure desktop submenus are closed when esc is pressed
+  const headerItems = document.querySelectorAll('.Header-menuItem');
+  headerItems.forEach(header => {
+    header.addEventListener('keyup', e => {
+      const event = e as KeyboardEvent;
+      if (event.key === 'Escape') {
+        (event.target as HTMLElement)?.blur();
+      }
+    });
+  });
+
+  // Mobile menu subnav menus
+  const headerbuttons = document.querySelectorAll('.js-headerMenuButton');
+  headerbuttons.forEach(button => {
     button.addEventListener('click', e => {
       e.preventDefault();
-      header?.classList.toggle('is-active');
-      button.setAttribute('aria-expanded', String(header?.classList.contains('is-active')));
+      const isActive = header?.classList.contains('is-active');
+      if (isActive) {
+        handleNavigationDrawerInactive(header);
+      } else {
+        handleNavigationDrawerActive(header);
+      }
+      button.setAttribute('aria-expanded', isActive ? 'true' : 'false');
     });
   });
 
   const scrim = document.querySelector('.js-scrim');
   scrim?.addEventListener('click', e => {
     e.preventDefault();
-    header?.classList.remove('is-active');
-    menuButtons.forEach(button => {
-      button.setAttribute('aria-expanded', String(header?.classList.contains('is-active')));
+
+    // find any active submenus and close them
+    const activeSubnavs = document.querySelectorAll('.go-NavigationDrawer-submenuItem.is-active');
+    activeSubnavs.forEach(subnav => handleNavigationDrawerInactive(subnav as HTMLElement));
+
+    handleNavigationDrawerInactive(header);
+
+    headerbuttons.forEach(button => {
+      button.setAttribute(
+        'aria-expanded',
+        header?.classList.contains('is-active') ? 'true' : 'false'
+      );
     });
   });
+
+  const getNavigationDrawerMenuItems = (navigationDrawer: HTMLElement): HTMLElement[] => {
+    if (!navigationDrawer) {
+      return [];
+    }
+
+    const menuItems = Array.from(
+      navigationDrawer.querySelectorAll(
+        ':scope > .go-NavigationDrawer-nav > .go-NavigationDrawer-list > .go-NavigationDrawer-listItem > a, :scope > .go-NavigationDrawer-nav > .go-NavigationDrawer-list > .go-NavigationDrawer-listItem > .go-Header-socialIcons > a'
+      ) || []
+    );
+    console.log(menuItems);
+
+    const anchorEl = navigationDrawer.querySelector('.go-NavigationDrawer-header > a');
+    if (anchorEl) {
+      menuItems.unshift(anchorEl);
+    }
+    return menuItems as HTMLElement[];
+  };
+
+  const getNavigationDrawerIsSubnav = (navigationDrawer: HTMLElement) => {
+    if (!navigationDrawer) {
+      return;
+    }
+    return navigationDrawer.classList.contains('go-NavigationDrawer-submenuItem');
+  };
+
+  const handleNavigationDrawerInactive = (navigationDrawer: HTMLElement) => {
+    if (!navigationDrawer) {
+      return;
+    }
+    const menuItems = getNavigationDrawerMenuItems(navigationDrawer);
+    navigationDrawer.classList.remove('is-active');
+    const parentMenuItem = navigationDrawer
+      .closest('.go-NavigationDrawer-listItem')
+      ?.querySelector(':scope > a') as HTMLElement;
+    parentMenuItem?.focus();
+    menuItems?.forEach(item => item?.setAttribute('tabindex', '-1'));
+    if (menuItems && menuItems[0]) {
+      menuItems[0].removeEventListener('keydown', handleMenuItemTabLeftFactory(navigationDrawer));
+      menuItems[menuItems.length - 1].removeEventListener(
+        'keydown',
+        handleMenuItemTabRightFactory(navigationDrawer)
+      );
+    }
+
+    if (navigationDrawer === header) {
+      headerbuttons && (headerbuttons[0] as HTMLElement)?.focus();
+    }
+  };
+
+  const handleNavigationDrawerActive = (navigationDrawer: HTMLElement) => {
+    const menuItems = getNavigationDrawerMenuItems(navigationDrawer);
+
+    navigationDrawer.classList.add('is-active');
+    menuItems.forEach(item => item.setAttribute('tabindex', '0'));
+    menuItems[0].focus();
+
+    menuItems[0].addEventListener('keydown', handleMenuItemTabLeftFactory(navigationDrawer));
+    menuItems[menuItems.length - 1].addEventListener(
+      'keydown',
+      handleMenuItemTabRightFactory(navigationDrawer)
+    );
+  };
+
+  const handleMenuItemTabLeftFactory = (navigationDrawer: HTMLElement) => {
+    return (e: KeyboardEvent) => {
+      if (e.key === 'Tab' && e.shiftKey) {
+        e.preventDefault();
+        handleNavigationDrawerInactive(navigationDrawer);
+      }
+    };
+  };
+
+  const handleMenuItemTabRightFactory = (navigationDrawer: HTMLElement) => {
+    return (e: KeyboardEvent) => {
+      if (e.key === 'Tab' && !e.shiftKey) {
+        e.preventDefault();
+        handleNavigationDrawerInactive(navigationDrawer);
+      }
+    };
+  };
+
+  const prepMobileNavigationDrawer = (navigationDrawer: HTMLElement) => {
+    const isSubnav = getNavigationDrawerIsSubnav(navigationDrawer);
+    const menuItems = getNavigationDrawerMenuItems(navigationDrawer);
+    navigationDrawer.addEventListener('keyup', e => {
+      if (e.key === 'Escape') {
+        handleNavigationDrawerInactive(navigationDrawer);
+      }
+    });
+
+    menuItems.forEach(item => {
+      const parentLi = item.closest('li');
+      if (parentLi && parentLi.classList.contains('js-mobile-subnav-trigger')) {
+        const submenu = parentLi.querySelector('.go-NavigationDrawer-submenuItem') as HTMLElement;
+        item.addEventListener('click', () => {
+          handleNavigationDrawerActive(submenu);
+        });
+      }
+    });
+    if (isSubnav) {
+      handleNavigationDrawerInactive(navigationDrawer);
+      navigationDrawer
+        ?.querySelector('.go-NavigationDrawer-header')
+        ?.addEventListener('click', e => {
+          e.preventDefault();
+          handleNavigationDrawerInactive(navigationDrawer);
+        });
+    }
+  };
+
+  document
+    .querySelectorAll('.go-NavigationDrawer')
+    .forEach(drawer => prepMobileNavigationDrawer(drawer as HTMLElement));
+
+  handleNavigationDrawerInactive(header);
 }
 
 export function registerSearchFormListeners(): void {
