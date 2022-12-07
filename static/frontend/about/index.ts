07ce07364aae6f3521716069ef00fa432e8ca6a1
Jamal Carvalho <jamal@golang.org>
2022-12-07 17:53:06 +0000

    static/frontend: add missing license headers
    Change-Id: Ibf822729bf7ad59ee16bc2808aec270e8820d489
    Reviewed-on: https://go-review.googlesource.com/c/pkgsite/+/455957
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- static/frontend/about/index.ts
+++ static/frontend/about/index.ts
@@ -1,4 +1,11 @@
 /**
+ * @license
+ * Copyright 2022 The Go Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+/**
  * Left Navigation.
  */
 export const initJumpLinks = async function () {
